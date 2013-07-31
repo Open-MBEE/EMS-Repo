098b7517e724790a58e80e21d54e3c194d4267e0
Doris T Lam <Doris.T.Lam@jpl.nasa.gov>
2013-07-31 10:56:21 -0700

    initial porting of md posting to ve

---
+++ src/main/amp/config/alfresco/extension/templates/webscripts/gov/nasa/jpl/view.post.js
@@ -0,0 +1,122 @@
+var modelFolder = roothome.childByNamePath("/Sites/europa/vieweditor/model");
+var presentationFolder = roothome.childByNamePath("/Sites/europa/vieweditor/presentation");
+
+function updateOrCreateModelElement(element, force) {
+	var modelNode = modelFolder.childrenByXPath("*[@view:mdid='" + element.mdid + "']");
+	if (modelNode == null || modelNode.length == 0) {
+		if (element.type == "View")
+			modelNode = parent.createNode(element.name, "view:View");
+		else if (element.type == "Property")
+			modelNode = parent.createNode(element.name, "view:Property");
+		else if (element.type == "Comment")
+			modelNode = parent.createNode("Comment", "view:Comment");
+		else
+			modelNode = parent.createNode(element.name, "view:ModelElement");
+	} else
+		modelNode = modelNode[0];
+
+	if (element.name != null && element.name != undefined && element.name != modelNode.properties["cm:name"])
+		if (force)
+			modelNode.properties["cm:name"] = element.name;
+		else
+			modelNode.properties["cm:name"] = modelNode.properties["cm:name"] + " - MERGED - " + element.name;
+	if (element.documentation != modelNode.properties["view:documentation"])
+		if (force)
+			modelNode.properties["view:documentation"] = element.documentation;
+		else
+			modelNode.properties["view:documentation"] = modelNode.properties["view:documentation"] + " <p><strong><i> MERGED NEED RESOLUTION! </i></strong></p> " + element.documentation;
+	if (element.type == "Property" && element.dvalue != modelNode.properties["view:dvalue"])
+		if (force)
+			modelNode.properties["view:dvalue"] = element.dvalue;
+		else
+			modelNode.properties["view:dvalue"] = modelNode.properties["view:dvalue"] + " - MERGED - " + element.dvalue;
+	modelNode.properties["view:mdid"] = element.mdid;
+	modelNode.save();
+	return modelNode;
+}
+
+function updateOrCreateView(view) {
+	var viewNode = modelFolder.childrenByXPath("*[@view:mdid='" + view.mdid + "']");
+	if (viewNode == null || modelNode.length == 0) {
+		return;
+	}
+	viewNode = viewNode[0];
+	var contains = viewNode.assocs["view:contains"];
+	for (var contain in contains) {
+		viewNode.removeAssociation(contains[contain], "view:contains");
+		contains[contain].remove();
+	}
+	var i = 1;
+	for (var j in view.contains) {
+		var containedObject = createContainedElement(view.contains[j], i);
+		viewNode.createAssociation(containedObject, "view:contains");
+		i++;
+	}
+	if (view.noSection != null && view.noSection != undefined)
+		viewNode.properties["view:noSection"] = view.noSection;
+	else
+		viewNode.properties["view:noSection"] = false;
+	viewNode.save();
+	return viewNode;
+}
+
+function updateViewHierarchy(views) {
+	
+}
+
+function createContainedElement(contained, i) {
+	var pnode = null;
+	if (contained.type == "Paragraph") {
+		pnode = presentationFolder.createNode("blah", "view:Paragraph");
+		if (contained.source == "text") {
+			tnode = presentationFolder.createNode("blah", "view:Text");
+			tnode.properties["view:text"] = contained.text;
+			tnode.save();
+			pnode.createAssociation(tnode, "view:source");
+		} else {
+			modelNode = modelFolder.childrenByXPath("*[@view:mdid='" + contained.source + "']");
+			pnode.createAssociation(modelNode, "view:source");
+			pnode.properties["view:useProperty"] = contained.useProperty;
+			pnode.save();
+		}
+	
+	} else if (contained.type == "Table") {
+		pnode = presentationFolder.createNode(contained.title, "view:Table");
+		pnode.properties["view:header"] = jsonUtils.toJSONString(contained.header);
+		pnode.properties["view:body"] = jsonUtils.toJSONString(contained.body);
+		pnode.properties["view:style"] = contained.style;
+		pnode.save();
+		
+	}
+	var orderable = new Array(1);
+	orderable["view:index"] = i;
+	pnode.addAspect("view:Orderable", orderable);
+	return pnode;
+}
+
+function main() {
+	//var europaSite = siteService.getSite("europa");
+	var postjson = jsonUtils.toObject(json);
+	var topview = modelFolder.childrenByXPath("*[@view:mdid='" + args['viewid'] + "']");
+	if (topview == null || topview.length == 0) {
+		if (args['doc'] == 'true') {
+			topview = modelFolder.createNode("blah", "view:DocumentView");
+		} else {
+			topview = modelFolder.createNode("blah", "view:View");
+		}
+		topview.properties["view:mdid"] = args['viewid'];
+		topview.save();
+	} 
+	var force = args['force'] == 'true' ? true : false;
+	for (var element in postjson.elements) {
+		updateOrCreateModelElement(postjson.elements[element], force);
+	}
+	for (var view in postjson.views) {
+		updateOrCreateView(postjson.views[view]);
+	}
+	if (args['recurse'] == 'true') {
+		updateViewHierarchy(postjson.view2view);
+	}	
+}
+
+main();
