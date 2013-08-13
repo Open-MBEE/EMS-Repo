bce3381af90adb2fc8d7fbbe2108c70d2537f7e6
Doris T Lam <Doris.T.Lam@jpl.nasa.gov>
2013-08-13 15:41:33 -0700

    comment get and post

--- src/main/amp/config/alfresco/extension/templates/webscripts/gov/nasa/jpl/viewcomment.get.js
+++ src/main/amp/config/alfresco/extension/templates/webscripts/gov/nasa/jpl/viewcomment.get.js
@@ -3,11 +3,37 @@
 var europaSite = siteService.getSite("europa").node;
 var modelFolder = europaSite.childByNamePath("/vieweditor/model");
 var res = [];
-var seen = [];
 
 var viewid = url.templateArgs.viewid;
 var recurse = args.recurse == 'true' ? true : false;
 
+var view2comment = {};
+var comments = [];
+
+function handleView(viewnode) {
+	var commentids = [];
+	var viewcomments = viewnode.assocs["view:comments"];;
+	for (var i in viewcomments) {
+		var commentNode = viewcomments[i];
+		if (commentNode.properties["view:committed"])
+			continue;
+		commentids.push(commentNode.properties["view:mdid"]);
+		var commentdetail = {
+				"id": commentNode.properties["view:mdid"], 
+				"body": commentNode.properties["view:documentation"],
+				"deleted": commentNode.properties["view:deleted"]
+		};//modified and author?
+		comments.push(commentdetail);
+	}
+	view2comment[viewnode.properties["view:mdid"]] = commentids;
+	
+	if (recurse) {
+		var cviews = viewnode.assocs["view:views"];
+		for (var i in cviews) {
+			handleView(cviews[i]);
+		}
+	}
+}
 
 function main() {
 	var topview = modelFolder.childrenByXPath("*[@view:mdid='" + viewid + "']");
@@ -18,11 +44,8 @@ function main() {
 		handleView(topview);
 	}
 }
-
-var response = "[]";
+var response = ""
 main();
-
-if (res.length > 0) {
-	response = jsonUtils.toJSONString(res);
-}
+if (response != "NotFound")
+	response = jsonUtils.toJSONString({"comments": comments, "view2comment": view2comment});
 model['res'] = response;
--- src/main/amp/config/alfresco/extension/templates/webscripts/gov/nasa/jpl/viewcomment.post.json.js
+++ src/main/amp/config/alfresco/extension/templates/webscripts/gov/nasa/jpl/viewcomment.post.json.js
@@ -8,19 +8,53 @@ var modelFolder = europaSite.childByNamePath("/vieweditor/model");
 var presentationFolder = europaSite.childByNamePath("/vieweditor/presentation");
 
 var modelMapping = {};
-var merged = [];
+
+
+function updateOrCreateComment(comment) {
+	var commentNode = modelFolder.childrenByXPath("*[@view:mdid='" + comment.id + "']");
+	if (commentNode == null || commentNode == undefined || commentNode.length == 0) {
+		commentNode = modelFolder.createNode(element.id, "view:Comment");
+	} else
+		commentNode = commentNode[0];
+	commentNode.properties["view:documentation"] = element.body;
+	//modified and authur??
+	commentNode.properties["view:deleted"] = false;
+	commentNode.properties["view:committed"] = true;
+	commentNode.save();
+	modelMapping[element.id] = commentNode;
+}
+
+function doView(viewid, comments) {
+	var viewnode = modelFolder.childrenByXPath("*[@view:mdid='" + viewid + "']");
+	if (viewnode == null || viewnode.length == 0)
+		return;
+	viewnode = viewnode[0];
+	curcomments = viewnode.assocs["view:comments"];
+	for (var i in curcomments) {
+		var commentNode = curcomments[i];
+		if (commentNode.properties["view:mdid"] in modelMapping) 
+			continue;//notfound?
+		if (commentNode.properties["view:committed"])
+			commentNode.properties['view:deleted'] = true;
+		commentNode.save();
+	}
+	for (var i in comments) {
+		var commentNode = modelMapping[comments[i]];
+		viewnode.createAssociation(commentNode, "view:comments");
+	}
+}
 
 function main() {
 	var postjson = JSON.parse(json.toString());
 	if (postjson == null || postjson == undefined)
 		return;
-	var viewid = url.templateArgs.viewid;
-	
+	for (var i in postjson.comments) {
+		updateOrCreateComment(postjson.comments[i]);
+	}
+	for (var viewid in postjson.views2comment) {
+		doView(viewid, postjson.view2comment[viewid]);
+	}
 }
 
 main();
-var response = "ok";
-if (merged.length > 0) {
-	response = jsonUtils.toJSONString(merged);
-}
-model['res'] = response;
+model['res'] = "ok";
