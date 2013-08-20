98983aa95bd4867206cbf2abcdc794f60c03c374
cinyoung <cinyoung@jpl.nasa.gov>
2013-08-20 14:09:55 -0700

    Refactored the artifact service to take path in as part of the URL.

--- src/main/amp/config/alfresco/extension/templates/webscripts/gov/nasa/jpl/artifacts.get.js
+++ src/main/amp/config/alfresco/extension/templates/webscripts/gov/nasa/jpl/artifacts.get.js
@@ -1,51 +1,88 @@
 // lets get all the arguments
-var id = null;
+var path = "";
 var extension = null;
 var cs = null;
 var filename = null;
 
-// Template Args exposes URL template paraemters (e.g., template Args)
-id = url.templateArgs.id;
-filename = id;
+main();
 
-// Query parameters on URL as exposed as args dictionary)
-if ("extension" in args) {
-  extension = args.extension;
-  filename += "." + extension;
+/**
+ * Main function for executing logic
+ */
+function main() {
+	//return not-found if file isn't matched
+	status.code=404;
+	
+	//Query parameters on URL as exposed as args dictionary)
+	if ("extension" in args) {
+	  extension = "." + args.extension;
+	}
+	if ("cs" in args) {
+	  cs = args.cs;
+	}
+	
+	//Template Args exposes URL template parameters (e.g., template Args)
+	var tokens = url.templateArgs.path.split("/");
+	if (tokens.length == 1) {
+		filename = tokens[0];
+		if (extension != null) {
+			filename += extension;
+		}
+		searchForMatches(filename);
+	} else {
+		path += "Sites/";
+		for (var ii = 0; ii < tokens.length - 1; ii++) {
+			path += tokens[ii] + "/";
+		}
+		filename = tokens[ii];
+		if (extension != null) {
+			filename += extension;
+		}
+		var node = companyhome.childByNamePath(path + filename);
+		if (node != null && checkCs(node, cs)) {
+			status.code = 200;
+		}
+	}
+	
+	if (status.code == 200) {
+	  status.message = "File " + filename + " found";
+	} else {
+	  if (cs) {
+	    status.message = "File " + filename + " with cs=" + cs + " not found.";
+	  } else {
+	    status.message = "File " + filename + " not found";
+	  }
+	}
+	
+	status.redirect = true;
 }
-if ("cs" in args) {
-  cs = args.cs;
+
+/**
+ * Utility function for global filename matching
+ */
+function searchForMatches(filename) {
+	// check for name matches
+	var searchString = "@cm\\:name:" + filename;
+
+	var results = search.luceneSearch(searchString);
+	if (results.length > 0) {
+	  for (result in results) {
+	    var r = results[result];
+	    if (checkCs(r, cs)) {
+	    	status.code = 200;
+	    	break;
+	    }
+	  }
+	}
 }
 
-// check for name matches
-var searchString = "@cm\\:name:" + filename;
-
-// return not-found if file isn't matched
-status.code=404;
-var results = search.luceneSearch(searchString);
-if (results.length > 0) {
-  for (result in results) {
-    var r = results[result];
-    if (cs) {
-        if (r.properties["view:cs"] == cs) {
-           status.code = 200;
-           break;
-        }
-     } else {
-        status.code = 200;
-        break;
-    }
-  }
+/**
+ * Utility function for checking whether checksums match
+ */
+function checkCs(node, cs) {
+	if (cs == null || node.properties.cs == cs) {
+		return true;
+	} else {
+		return false;
+	}
 }
-
-if (status.code == 200) {
-  status.message = "File " + filename + " found";
-} else {
-  if (cs) {
-    status.message = "File " + filename + " with cs=" + cs + " not found";
-  } else {
-    status.message = "File " + filename + " not found";
-  }
-}
-
-status.redirect = true;
--- src/main/amp/config/alfresco/extension/templates/webscripts/gov/nasa/jpl/artifacts.post.js
+++ src/main/amp/config/alfresco/extension/templates/webscripts/gov/nasa/jpl/artifacts.post.js
@@ -1,75 +1,82 @@
-var PATH = "path";
-
+// everything is relative to Sites path
+var path = "Sites/";
+var extension = null;
+var cs = null;
 var filename = undefined;
 var content = undefined;
-var title = "";
-var description = "";
-var path = "";
 
+main();
 
-// locate file attributes from form
-for each (field in formdata.fields)
-{
-  if (field.name == "title")
-  {
-	  title = field.value;
-  }
-  else if (field.name == "desc")
-  {
-	  description = field.value;
-  }
-  else if (field.name == "file" && field.isFile)
-  {
-	  filename = field.filename;
-	  content = field.content;
-  }
-  else if (field.name = PATH) 
-  {
-	  path = field.path;
-  } 
-}
-if (PATH in args) 
-{
-	path = args.path;
-}
-// lets make sure path ends with "/"
-var pathTokens = path.split("/");
-path = "";
-for (key in pathTokens) {
-	path += pathTokens[key] + "/";
-}
-
-
-// ensure mandatory file attributes have been located
-if (filename == undefined || content == undefined)
-{
-	status.code = 400;
-	status.message = "Uploaded file cannot be located in request";
-	status.redirect = true;
-}
-else
-{
-	upload = companyhome.childByNamePath(path + filename);
-	if (upload == null) {
-		var node = mkdir(path);
-		upload = node.createFile(filename) ;
+function main() {
+	//Query parameters on URL as exposed as args dictionary)
+	if ("extension" in args) {
+	  extension = "." + args.extension;
 	}
-    
-	upload.properties.content.write(content);
-	upload.properties.content.setEncoding("UTF-8");
-	upload.properties.content.guessMimetype(filename);
-  
-	upload.properties.title = title;
-	upload.properties.description = description;
-	upload.save();
- 
-	// setup model for response template
-	model.upload = upload;
-	status.code = 200;
+	if ("cs" in args) {
+	  cs = args.cs;
+	}
+	for each (field in formdata.fields) {
+		if (field.name == "content" && field.isFile) {
+			content = field.content;
+		}
+	}
+	
+	//Template Args exposes URL template parameters (e.g., template Args)
+	var tokens = url.templateArgs.path.split("/");
+	if (tokens.length == 1) {
+		filename = tokens[0];
+		if (extension != null) {
+			filename += extension;
+		}
+	} else {
+		for (var ii = 0; ii < tokens.length - 1; ii++) {
+			path += tokens[ii] + "/";
+		}
+		filename = tokens[ii];
+		if (extension != null) {
+			filename += extension;
+		}
+	}
+	
+	// ensure mandatory file attributes have been located
+	if (filename == undefined || content == undefined)
+	{
+		status.code = 400;
+		status.message = "Uploaded file cannot be located in request";
+		status.redirect = true;
+	}
+	else
+	{
+		upload = companyhome.childByNamePath(path + filename);
+		if (upload == null) {
+			var node = mkdir(path);
+			upload = node.createFile(filename) ;
+			
+			// TODO: check for aspect and create if necessary
+		}
+	    
+		upload.properties.content.write(content);
+		upload.properties.content.setEncoding("UTF-8");
+		upload.properties.content.guessMimetype(filename);
+	  
+		upload.properties.title = filename;
+//		upload.properties.description = description;
+		upload.save();
+	 
+		// setup model for response template
+		model.upload = upload;
+		status.code = 200;
+	}
 }
 
+//
+// Utility for making the path
+//
 function mkdir(path) 
 {
+	if (path.charAt(path.length) == "/") {
+		path = path.substr(0, path.length-1);
+	}
 	var tokens = path.split("/");
     var curPath = "";
     var prevPath = "";
