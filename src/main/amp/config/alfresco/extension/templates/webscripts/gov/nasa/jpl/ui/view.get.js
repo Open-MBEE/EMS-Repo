e9d59e78d5503df68eb8877498430edf43104258
Doris T Lam <Doris.T.Lam@jpl.nasa.gov>
2013-08-26 11:52:47 -0700

    view getting and posting from ui

---
+++ src/main/amp/config/alfresco/extension/templates/webscripts/gov/nasa/jpl/ui/view.get.js
@@ -0,0 +1,102 @@
+<import resource="classpath:alfresco/extension/js/json2.js">
+<import resource="classpath:alfresco/extension/js/utils.js">
+
+var europaSite = siteService.getSite("europa").node;
+var modelFolder = europaSite.childByNamePath("/vieweditor/model");
+var elements = [];
+var seen = [];
+var views = [];
+var view2view = {};
+
+var viewid = url.extension
+
+function addElement(modelNode) {
+	var info = {};
+	info['mdid'] = modelNode.properties["view:mdid"];
+	info['documentation'] = modelNode.properties["view:documentation"];
+	var name = modelNode.properties["view:name"];
+	if (name != null && name != undefined)
+		info['name'] = name;
+	var dvalue = modelNode.properties["view:defaultValue"];
+	if (dvalue != null && dvalue != undefined)
+		info['dvalue'] = dvalue;
+	elements.push(info);
+	seen.push(modelNode.properties['view:mdid']);
+}
+
+
+function handleView(view) {
+	var sourcesJson = view.properties["view:sourcesJson"];
+	var sources = JSON.parse(sourcesJson);
+	for (var i in sources) {
+		var sourceid = sources[i];
+		var modelNode = modelFolder.childrenByXPath("*[@view:mdid='" + sourceid + "']");
+		if (modelNode == null || modelNode == undefined)
+			continue;
+		if (seen.indexOf(sourceid) >= 0)
+			continue;
+		addElement(modelNode[0]);
+	}
+	
+	var viewinfo = {};
+	viewinfo['mdid'] = view.properties['view:mdid'];
+	viewinfo['noSection'] = view.properties['view:noSection'];
+	viewinfo['contains'] = JSON.parse(view.properties['view:containsJson']);
+	
+	var viewcomments = [];
+	var comments = view.assocs['view:comments'];
+	for (var i in comments) {
+		var comment = comments[i];
+		if (comment.properties['view:deleted'])
+			continue;
+		var commentinfo = {};
+		commentinfo['author'] = comment.properties['view:author'];
+		commentinfo['modified'] = utils.toISO8601(comment.properties['view:lastModified']);
+		commentinfo['mdid'] = comment.properties["view:mdid"];
+		commentinfo['body'] = comment.properties['view:documentation'];
+		viewcomments.push(commentinfo);
+	}
+	viewinfo['comments'] = viewcomments.sort(function(a,b) {
+		if (a.modified < b.modified)
+			return -1;
+		if (a.modified == b.modified)
+			return 0;
+		return 1;
+	});
+	
+	viewinfo['author'] = view.properties['view:author'];
+	viewinfo['modified'] = utils.toISO8601(view.properties['view:lastModified']);
+	views.push(viewinfo);
+	
+	view2view[view.properties['view:mdid']] = JSON.parse(view.properties['view:viewsJson']);
+	
+	var childViews = view.assocs["view:views"];
+	for (var i in childViews) {
+		handleView(childViews[i]);
+	}
+}
+
+function main() {
+	var topview = modelFolder.childrenByXPath("*[@view:mdid='" + viewid + "']");
+	if (topview == null || topview.length == 0) {
+		status.code = 404;
+	} else {
+		topview = topview[0];
+		handleView(topview);
+	}
+}
+
+status.code = 200;
+main();
+
+var info = {};
+info['elements'] = elements;
+info['view2view'] = view2view;
+info['views'] = views;
+info['rootView'] = viewid;
+info['user'] = person.properties['cm:userName'];
+
+var	response = status.code == 200 ? jsonUtils.toJSONString(info) : "NotFound";
+if (status.code != 200)
+	status.redirect = true;
+model['res'] = response;
---
+++ src/main/amp/config/alfresco/extension/templates/webscripts/gov/nasa/jpl/ui/view.post.json.js
@@ -0,0 +1,45 @@
+<import resource="classpath:alfresco/extension/js/json2.js">
+<import resource="classpath:alfresco/extension/js/utils.js">
+
+var europaSite = siteService.getSite("europa").node;
+var modelFolder = europaSite.childByNamePath("/vieweditor/model");
+var date = new Date();
+var user = person?;
+
+function updateModelElement(element) {
+	var modelNode = modelFolder.childrenByXPath("*[@view:mdid='" + element.mdid + "']");
+	if (modelNode == null || modelNode.length == 0) {
+		return;
+	} else
+		modelNode = modelNode[0];
+
+	if (element.name != null && element.name != undefined && element.name != modelNode.properties["view:name"]) {
+		modelNode.properties["view:name"] = element.name;
+	}
+	if (element.documentation != modelNode.properties["view:documentation"]) {
+		modelNode.properties["view:documentation"] = element.documentation;
+	}
+	if (element.dvalue != null && element.dvalue != undefined && element.dvalue != modelNode.properties["view:defaultValue"]) {
+		modelNode.properties["view:defaultValue"] = element.dvalue;
+	}
+	modelNode.save();
+}
+
+function main() {
+	var elements = JSON.parse(json.toString());
+	if (elements == null || elements == undefined)
+		return;
+	
+	for (var i in elements) {
+		updateModelElement(elements[i]);
+	}
+	
+	var viewid = url.templateArgs.viewid;
+	var viewnode = modelFolder.childrenByXPath("*[@view:mdid='" + viewid + "']");
+	viewnode.properties['author'] = person.properties['cm:userName'];
+	viewnode.properties['lastModified'] = date;
+	viewnode.save();
+}
+
+main();
+model['res'] = "ok";
