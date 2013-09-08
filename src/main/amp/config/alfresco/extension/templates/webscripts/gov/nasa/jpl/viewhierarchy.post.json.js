463e3d9b973fb5476fd74d77eaf395a1e4fc04b8
Doris Lam <dlamoris@gmail.com>
2013-09-07 23:05:28 -0700

    added support for product in posting view hierarchy

--- src/main/amp/config/alfresco/extension/templates/webscripts/gov/nasa/jpl/viewhierarchy.post.json.js
+++ src/main/amp/config/alfresco/extension/templates/webscripts/gov/nasa/jpl/viewhierarchy.post.json.js
@@ -15,8 +15,21 @@ function main() {
 	var viewid = url.templateArgs.viewid;
 	var views = postjson.views;
 	var nosections = postjson.noSections;
+	
+	var viewNode = modelFolder.childrenByXPath("*[@view:mdid='" + viewid + "']");
+	if (viewNode == null || viewNode.length == 0) {
+		return; //should throw error
+	}
+	viewNode = viewNode[0];
+	if (viewNode.properties["view:product"]) {
+		viewNode.properties["view:view2viewJson"] = jsonUtils.toJSONString(views);
+		viewNode.properties["view:noSectionsJson"] = jsonUtils.toJSONString(nosections);
+		viewNode.save();
+		return;
+	}
+
 	for (var viewid in views) {
-		var viewNode = modelFolder.childrenByXPath("*[@view:mdid='" + viewid + "']");
+		viewNode = modelFolder.childrenByXPath("*[@view:mdid='" + viewid + "']");
 		if (viewNode == null || viewNode.length == 0) {
 			continue;//should throw error
 		}
