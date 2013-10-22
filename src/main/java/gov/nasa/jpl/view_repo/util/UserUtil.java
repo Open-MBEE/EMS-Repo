27b52df8e7c5bf37e6c1ccf11ea6bf954376c4cd
cinyoung <cinyoung@jpl.nasa.gov>
2013-10-21 17:06:59 -0700

    Added in Java webscript utility with webscript root object UserUtil for checking permissions based on site membership - UserUtil.hasWebScriptPermissions.
    For running with Jetty if site isn't defined with memberships (via
    share, not just repo), then permissions will be false. To skip in
    testing update the service-context.xml to set ignore to true for the
    UserUtil bean.

---
+++ src/main/java/gov/nasa/jpl/view_repo/util/UserUtil.java
@@ -0,0 +1,54 @@
+package gov.nasa.jpl.view_repo.util;
+
+import org.alfresco.repo.processor.BaseProcessorExtension;
+import org.alfresco.repo.security.authentication.AuthenticationUtil;
+import org.alfresco.service.cmr.security.PersonService;
+import org.alfresco.service.cmr.site.SiteService;
+
+
+/**
+ * Simple utility for user testing functionality
+ * 
+ * @author cinyoung
+ *
+ */
+public class UserUtil extends BaseProcessorExtension {
+	// alternatively could use the more intuitive ScriptSiteService - siteServiceScript bean
+	private static SiteService siteService;
+	private static PersonService personService;
+	private static String siteName;
+	
+	// for local testing without Site created from share, set ignore to true in configuration so check isn't made
+	private static boolean ignore = false;
+	
+	public void setIgnore(boolean value) {
+		ignore = value;
+	}
+	
+	public void setPersonService(PersonService service) {
+		personService = service;
+	}
+	
+	public void setSiteService(SiteService service) {
+		siteService = service;
+	}
+	
+	public void setSiteName(String name) {
+		siteName = name;
+	}	
+	
+	/**
+	 * Checks whether or not the currently authenticated user has permissions to the
+	 * statically specified site.
+	 * @return
+	 */
+	public static boolean hasWebScriptPermissions() {
+		if (ignore) {
+			return true;
+		} else {
+			String username = AuthenticationUtil.getFullyAuthenticatedUser();
+			String normalized = personService.getUserIdentifier(username);
+			return siteService.isMember(siteName, normalized);
+		}
+	}
+}
