6bb8fa0277ba9e879ff3de00fd0b04c91bd38296
cinyoung <cinyoung@jpl.nasa.gov>
2013-11-18 17:20:35 -0800

    Working java backed webscript replacement of view.post.json.js. Uses foundational API at moment, have hooks to switch to just using scriptnode if necessary.

---
+++ src/main/java/gov/nasa/jpl/view_repo/webscripts/AbstractJavaWebScript.java
@@ -0,0 +1,187 @@
+package gov.nasa.jpl.view_repo.webscripts;
+
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.alfresco.model.ContentModel;
+import org.alfresco.repo.jscript.ScriptNode;
+import org.alfresco.repo.model.Repository;
+import org.alfresco.service.ServiceRegistry;
+import org.alfresco.service.cmr.repository.ChildAssociationRef;
+import org.alfresco.service.cmr.repository.NodeService;
+import org.alfresco.service.namespace.NamespaceService;
+import org.alfresco.service.namespace.QName;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.extensions.webscripts.DeclarativeWebScript;
+
+/**
+ * Base class for all EMS Java backed webscripts. Provides helper functions and
+ * key variables necessary for execution. This provides most of the capabilities
+ * that were in utils.js
+ * 
+ * @author cinyoung
+ * 
+ */
+public abstract class AbstractJavaWebScript extends DeclarativeWebScript {
+	// injected members
+	protected ServiceRegistry services;
+	protected Repository repository;
+
+	// internal members
+	protected NodeService nodeService;
+	protected ScriptNode companyhome;
+	protected final static String NAMESPACE_BEGIN = "" + QName.NAMESPACE_BEGIN;
+
+	protected final static boolean USE_FOUNDATIONAL = true;
+
+	@SuppressWarnings("serial")
+	protected final Map<String, String> typeMap = new HashMap<String, String>() {
+		{
+			put("View", "view:View");
+			put("Property", "view:Property");
+			put("Comment", "view:Comment");
+			put("ModelElement", "view:ModelElement");
+		}
+	};
+
+	protected void initMemberVariables(String siteName) {
+		this.nodeService = services.getNodeService();
+		companyhome = new ScriptNode(repository.getCompanyHome(), services);
+	}
+
+	public void setRepositoryHelper(Repository repositoryHelper) {
+		this.repository = repositoryHelper;
+	}
+
+	public void setServices(ServiceRegistry registry) {
+		this.services = registry;
+	}
+
+	protected void updateViewHierarchy(Map<String, ScriptNode> modelMapping,
+			JSONObject views) throws JSONException {
+		updateViewHierarchy(modelMapping, views, null);
+	}
+
+	protected void updateViewHierarchy(Map<String, ScriptNode> modelMapping,
+			JSONObject views, List<String> nosections) throws JSONException {
+		Iterator<?> keys = views.keys();
+		while (keys.hasNext()) {
+			String pview = (String) keys.next();
+			JSONArray cviews = views.getJSONArray(pview);
+			if (!modelMapping.containsKey(pview)) {
+				continue;
+			}
+			ScriptNode pviewnode = modelMapping.get(pview);
+			JSONArray oldchildren = (JSONArray) pviewnode.getAssocs().get(
+					"view:views");
+			if (oldchildren == null) {
+				continue;
+			}
+			for (int ii = 0; ii < oldchildren.length(); ii++) {
+				pviewnode.removeAssociation((ScriptNode) oldchildren.get(ii),
+						"view:views");
+			}
+			for (int ci = 0; ci < cviews.length(); ci++) {
+				String cvid = cviews.getString(ci);
+				ScriptNode cviewnode = modelMapping.get(cvid);
+				if (cviewnode == null) {
+					continue;
+				}
+				setNodeProperty(cviewnode, "view:index", ci);
+				pviewnode.createAssociation(cviewnode, "view:views");
+			}
+			setNodeProperty(pviewnode, "view:viewsJson", cviews.toString());
+			if (nosections != null && nosections.contains(pview)) {
+				setNodeProperty(pviewnode, "view:noSection", true);
+			}
+		}
+	}
+
+	/**
+	 * Clear all parent volume associations
+	 * 
+	 * @param dnode
+	 */
+	protected void cleanDocument(ScriptNode dnode) {
+		// TODO implement cleanDocument
+		dnode.getSourceAssocs().get("view:documents");
+	}
+
+	protected void setName(ScriptNode node, String name) {
+		setNodeProperty(node, "view:name", name);
+		setNodeProperty(node, "cm:title", name);
+	}
+
+	/**
+	 * Utility function to switch between different ways to set a NodeRefs
+	 * property
+	 * 
+	 * @param node
+	 * @param type
+	 * @param value
+	 */
+	protected void setNodeProperty(ScriptNode node, String type,
+			Serializable value) {
+		if (USE_FOUNDATIONAL) {
+			nodeService
+					.setProperty(node.getNodeRef(), createQName(type), value);
+		} else {
+			node.getProperties().put(type, value);
+			node.save();
+		}
+	}
+
+	protected Object getNodeProperty(ScriptNode node, String key) {
+		if (USE_FOUNDATIONAL) {
+			return nodeService.getProperty(node.getNodeRef(), createQName(key));
+		} else {
+			return node.getProperties().get(key);
+		}
+	}
+
+	protected ScriptNode getModelElement(ScriptNode parent, String name) {
+		return parent.childByNamePath(name);
+	}
+
+	protected ScriptNode createModelElement(ScriptNode parent, String name,
+			String type) {
+		if (USE_FOUNDATIONAL) {
+			Map<QName, Serializable> props = new HashMap<QName, Serializable>(
+					1, 1.0f);
+			props.put(ContentModel.PROP_NAME, name);
+
+			ChildAssociationRef assoc = nodeService.createNode(parent
+					.getNodeRef(), ContentModel.ASSOC_CONTAINS, QName
+					.createQName(NamespaceService.CONTENT_MODEL_1_0_URI,
+							QName.createValidLocalName(name)),
+					createQName(type), props);
+			return new ScriptNode(assoc.getChildRef(), services);
+		} else {
+			return parent.createNode(name, type);
+		}
+	}
+
+	/**
+	 * Helper to create a QName from either a fully qualified or short-name
+	 * QName string
+	 * 
+	 * @param s
+	 *            Fully qualified or short-name QName string
+	 * 
+	 * @return QName
+	 */
+	protected QName createQName(String s) {
+		QName qname;
+		if (s.indexOf(NAMESPACE_BEGIN) != -1) {
+			qname = QName.createQName(s);
+		} else {
+			qname = QName.createQName(s, this.services.getNamespaceService());
+		}
+		return qname;
+	}
+}
--- src/main/java/gov/nasa/jpl/view_repo/webscripts/ViewPostJson.java
+++ src/main/java/gov/nasa/jpl/view_repo/webscripts/ViewPostJson.java
@@ -1,7 +1,8 @@
 package gov.nasa.jpl.view_repo.webscripts;
 
-import gov.nasa.jpl.view_repo.util.UserUtil;
+//import gov.nasa.jpl.view_repo.util.UserUtil;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -19,7 +20,7 @@ import org.springframework.extensions.webscripts.Cache;
 import org.springframework.extensions.webscripts.Status;
 import org.springframework.extensions.webscripts.WebScriptRequest;
 
-public class ViewPostJson extends AbstractWebScript {
+public class ViewPostJson extends AbstractJavaWebScript {
 	private ScriptNode modelFolder = null;
 	// snapshotFolder not needed here
 	private Map<String, ScriptNode> modelMapping;
@@ -46,8 +47,8 @@ public class ViewPostJson extends AbstractWebScript {
 
 
 	private ScriptNode updateOrCreateModelElement(JSONObject element, boolean force) throws JSONException {
-		String mdid = (String) element.get("mdid");
-		String elementType = (String) element.get("type");
+		String mdid = element.has("mdid") ? (String) element.get("mdid") : null;
+		String elementType = element.has("type") ? (String) element.get("type") : null;
 		String elementName = element.has("name") ? (String) element.getString("name") : null;
 
 		ScriptNode modelNode = modelMapping.get(mdid);
@@ -61,14 +62,13 @@ public class ViewPostJson extends AbstractWebScript {
 					setName(modelNode, elementName);
 				}
 			}
-			modelNode.save();
 		}
 		
-		if (elementName != null && !elementName.equals(modelNode.getProperties().get("view:name"))) {
+		if (elementName != null && !elementName.equals(getNodeProperty(modelNode, "view:name"))) {
 			if (force) {
 				setName(modelNode, elementName);
 			} else {
-				setName(modelNode, modelNode.getProperties().get("view:name") + " - MERGED - " + elementName);
+				setName(modelNode, getNodeProperty(modelNode, "view:name") + " - MERGED - " + elementName);
 			}
 			Map<String, String> mergedEntry = new HashMap<String, String>();
 			mergedEntry.put("mdid", mdid);
@@ -76,12 +76,12 @@ public class ViewPostJson extends AbstractWebScript {
 			merged.add(mergedEntry);
 		}
 		
-		String elementDocumentation = (String) element.get("documentation");
-		if (!elementDocumentation.equals(modelNode.getProperties().get("view:documentation"))) {
+		String elementDocumentation = element.has("documentation") ? (String) element.get("documentation") : null;
+		if (elementDocumentation != null && !elementDocumentation.equals(getNodeProperty(modelNode, "view:documentation"))) {
 			if (force) {
-				modelNode.getProperties().put("view:documentation", elementDocumentation);
+				setNodeProperty(modelNode, "view:documentation", elementDocumentation);
 			} else {
-				modelNode.getProperties().put("view:documentation", modelNode.getProperties().get("view:documentation") + " <p><strong><i> MERGED NEED RESOLUTION! </i></strong></p> " + elementDocumentation);
+				setNodeProperty(modelNode, "view:documentation", getNodeProperty(modelNode, "view:documentation") + " <p><strong><i> MERGED NEED RESOLUTION! </i></strong></p> " + elementDocumentation);
 			}
 			Map<String, String> mergedEntry = new HashMap<String, String>();
 			mergedEntry.put("mdid", mdid);
@@ -89,12 +89,12 @@ public class ViewPostJson extends AbstractWebScript {
 			merged.add(mergedEntry);
 		}
 		
-		String dvalue = (String)element.get("dvalue");
-		if (elementType.equals("Property") && !dvalue.equals(modelNode.getProperties().get("view:defaultValue"))) {
+		String dvalue = element.has("dvalue") ? (String)element.get("dvalue") : null;
+		if (elementType != null && elementType.equals("Property") && dvalue != null && !dvalue.equals(getNodeProperty(modelNode, "view:defaultValue"))) {
 			if (force) {
-				modelNode.getProperties().put("view:defaultValue", dvalue);
+				setNodeProperty(modelNode, "view:defaultValue", dvalue);
 			} else {
-				modelNode.getProperties().put("view:defaultValue", modelNode.getProperties().get("view:defaultValue") + " - MERGED - " + dvalue);
+				setNodeProperty(modelNode, "view:defaultValue", getNodeProperty(modelNode, "view:defaultValue") + " - MERGED - " + dvalue);
 			}
 			Map<String, String> mergedEntry = new HashMap<String, String>();
 			mergedEntry.put("mdid", mdid);
@@ -102,8 +102,7 @@ public class ViewPostJson extends AbstractWebScript {
 			merged.add(mergedEntry);
 		}
 		
-		modelNode.getProperties().put("view:mdid", mdid);
-		modelNode.save();
+		setNodeProperty(modelNode, "view:mdid", mdid);
 		modelMapping.put(mdid, modelNode);
 		return modelNode;
 	}
@@ -127,34 +126,33 @@ public class ViewPostJson extends AbstractWebScript {
 			fillSources((JSONObject)array.get(ii), sources);
 		}
 		array = new JSONArray(sources);
-		viewNode.getProperties().put("view:sourcesJson", array.toString());
+		setNodeProperty(viewNode, "view:sourcesJson", array.toString());
 		
 		if (view.get("noSection") != null && !ignoreNoSection) {
-			viewNode.getProperties().put("view:noSection", view.get("noSection"));
+			setNodeProperty(viewNode, "view:noSection", (Serializable)view.get("noSection"));
 		} else {
-			viewNode.getProperties().put("view:noSection", false);
+			setNodeProperty(viewNode, "view:noSection", false);
 		}
 
-		viewNode.getProperties().put("view:containsJson", view.getJSONArray("contains").toString());
-		viewNode.getProperties().put("view:author", user);
-		viewNode.getProperties().put("view:lastModified", new Date());
-		viewNode.save();
+		setNodeProperty(viewNode, "view:containsJson", view.getJSONArray("contains").toString());
+		setNodeProperty(viewNode, "view:author", user);
+		setNodeProperty(viewNode, "view:lastModified", new Date());
 		
 		return viewNode;
 	}
 
 
 	private void fillSources(JSONObject contained, List<String> sources) throws JSONException {
-		String type = (String)contained.get("type");
-		String source = (String)contained.get("source");
+		String type = contained.has("type") ? (String)contained.get("type") : null;
+		String source = contained.has("source") ? (String)contained.get("source") : null;
 		
-		if (type.equals("Paragraph")) {
-			if (source.equals("text")) {
+		if (type != null && type.equals("Paragraph")) {
+			if (source == null || source.equals("text")) {
 				// do nothing
 			} else {
 				ScriptNode modelNode = modelMapping.get(source);
-				if (!sources.contains((String)modelNode.getProperties().get("view:mdid"))) {
-					sources.add((String)modelNode.getProperties().get("view:mdid"));
+				if (!sources.contains((String)getNodeProperty(modelNode, "view:mdid"))) {
+					sources.add((String)getNodeProperty(modelNode, "view:mdid"));
 				}
 			}
 		} else if (type.equals("Table") || type.equals("List")) {
@@ -179,8 +177,9 @@ public class ViewPostJson extends AbstractWebScript {
 	/**
 	 * Main execution method
 	 * @param req
+	 * @throws InterruptedException 
 	 */
-	private void mainMethod(WebScriptRequest req) {
+	private void mainMethod(WebScriptRequest req) throws InterruptedException {
 		JSONObject postjson = null;
 		String viewid = null; 
 
@@ -207,25 +206,23 @@ public class ViewPostJson extends AbstractWebScript {
 			product = true;
 		}
 		if (topview == null) {
-			if (req.getParameter("doc").equals("true")) {
+			if (checkArgEquals(req, "doc", "true")) {
 				topview = createModelElement(modelFolder, viewid, "view:DocumentView");
 				if (product) {
-					topview.getProperties().put("view:product", true);
+					setNodeProperty(topview, "view:product", true);
 				}
 			} else {
 				topview = createModelElement(modelFolder, viewid, "view:View");
 			}
-			topview.getProperties().put("view:mdid", viewid);
-			topview.save();
+			setNodeProperty(topview, "view:mdid", viewid);
 		}
 		modelMapping.put(viewid, topview);
 		
 		if ( !topview.getTypeShort().equals("view:DocumentView") && checkArgEquals(req, "doc", "true") ) {
 			topview.specializeType("view:DocumentView");
 			if (product) {
-				topview.getProperties().put("view:product", true);
+				setNodeProperty(topview, "view:product", true);
 			}
-			topview.save();
 		}
 		boolean force = checkArgEquals(req, "force", "true") ? true : false;
 		
@@ -243,7 +240,7 @@ public class ViewPostJson extends AbstractWebScript {
 			}
 			
 			if (checkArgEquals(req, "recurse", "true") && !product) {
-				updateViewHierarchy(modelMapping, postjson.getJSONArray("view2view"));
+				updateViewHierarchy(modelMapping, postjson.getJSONObject("view2view"));
 			}
 			
 			if (product) {
@@ -254,11 +251,10 @@ public class ViewPostJson extends AbstractWebScript {
 						noSections.add((String)view.get("mdid"));
 					}
 				}
-				topview.getProperties().put("view:view2viewJson", postjson.getJSONArray("view2view").toString());
+				setNodeProperty(topview, "view:view2viewJson", postjson.getJSONArray("view2view").toString());
 				
 				JSONArray nosectionjsa = new JSONArray(noSections);
-				topview.getProperties().put("view:noSectionsJson", nosectionjsa.toString());
-				topview.save();
+				setNodeProperty(topview, "view:noSectionsJson", nosectionjsa.toString());
 			}
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
@@ -276,12 +272,17 @@ public class ViewPostJson extends AbstractWebScript {
 		String response;
 	
 		// check user site permissions for webscript
-		if (UserUtil.hasWebScriptPermissions()) {
+//		if (UserUtil.hasWebScriptPermissions()) {
 			status.setCode(HttpServletResponse.SC_OK);
-			mainMethod(req);
-		} else {
-			status.setCode(HttpServletResponse.SC_UNAUTHORIZED);
-		}
+			try {
+				mainMethod(req);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+//		} else {
+//			status.setCode(HttpServletResponse.SC_UNAUTHORIZED);
+//		}
 		
 		// set response based on status
 		switch (status.getCode()) {
