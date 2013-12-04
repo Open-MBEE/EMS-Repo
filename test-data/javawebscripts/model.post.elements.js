6158bf3307d26a4e00780664f420952d7188fa46
cinyoung <cinyoung@jpl.nasa.gov>
2013-12-04 09:29:29 -0800

    intermediate commit of model element posting functionality

---
+++ test-data/javawebscripts/model.post.elements.js
@@ -0,0 +1,12 @@
+{
+    "elements" : {
+        "111_1" : {
+            "type" : "Package",
+            "name" : "Test 1"
+        },
+        "111_2" : {
+            "type" : "Property",
+            "name" : "Test 2"
+        }
+    }
+}
