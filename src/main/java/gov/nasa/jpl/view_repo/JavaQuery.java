a47ece45344819423ac547180a4522d6d70dcfa9
Bradley J Clement <Bradley.J.Clement@jpl.nasa.gov>
2013-12-30 15:38:56 -0800

    minor

--- src/main/java/gov/nasa/jpl/view_repo/JavaQuery.java
+++ src/main/java/gov/nasa/jpl/view_repo/JavaQuery.java
@@ -40,6 +40,7 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
