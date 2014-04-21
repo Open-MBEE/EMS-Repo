0435830ca0288d31095314f9ce808d71a0e6cd8b
bclement <bclement@jpl.nasa.gov>
2014-04-21 08:51:34 -0700

    adding Viewable List; subclasses List in the sysml repository

---
+++ src/main/java/gov/nasa/jpl/view_repo/sysml/List.java
@@ -0,0 +1,71 @@
+package gov.nasa.jpl.view_repo.sysml;
+
+import gov.nasa.jpl.view_repo.util.EmsScriptNode;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import sysml.Viewable;
+
+public class List extends ArrayList< Viewable< EmsScriptNode > > implements sysml.List< EmsScriptNode > {
+
+    private static final long serialVersionUID = 3954654861037876503L;
+
+    /**
+     * Create an empty List.
+     * @see java.util.List()
+     */
+    public List() {
+        super();
+    }
+
+    /**
+     * Create a List and add the {@link Viewable}s in the input {@link Collection}.
+     * @param c
+     * @see java.util.List(Collection< ? extends Viewable >)
+     */
+    public List( Collection< ? extends Viewable< EmsScriptNode > > c ) {
+        super( c );
+    }
+
+    /**
+     * Create a List with a given number of null entries. 
+     * @param initialCapacity
+     * @see java.util.List(int)
+     */
+    public List( int initialCapacity ) {
+        super( initialCapacity );
+    }
+
+    /* 
+     * <code>
+     *       {
+     *           "type": "List",
+     *           "list": [
+     *               [{ //each list item can have multiple things, Table in a list may not be supported
+     *                   "type": "Paragraph"/"List"/"Table",
+     *                   (see above...)
+     *               }, ...], ...
+     *           ],
+     *           "ordered": true/false
+     *       }
+     * </code>
+     * @returns a String in the JSON format above
+     * @see sysml.Viewable#toViewJson()
+     */
+    @Override
+    public String toViewJson() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /* (non-Javadoc)
+     * @see sysml.Viewable#getDisplayedElements()
+     */
+    @Override
+    public Collection< EmsScriptNode > getDisplayedElements() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
