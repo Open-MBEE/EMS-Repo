bb9eb2ab323f67c4ec2f1f9d5622db18755c50b2
gcgandhi <gorang.c.gandhi@jpl.nasa.gov>
2014-04-22 15:08:13 -0700

    Updated content model for changes to Operation and ViewPoint for the demo.  Minor updated to View to get the expression property, not the Expression property.

--- src/main/java/gov/nasa/jpl/view_repo/sysml/View.java
+++ src/main/java/gov/nasa/jpl/view_repo/sysml/View.java
@@ -197,7 +197,7 @@ public class View extends List implements sysml.View< EmsScriptNode > {
         //   model.getPropertyWithType( viewpointMethod,
         //                              getTypeWithName(null, "Expression" ) );
         Collection< EmsScriptNode > viewpointExprs =
-                model.getProperty( viewpointMethod, Acm.JSON_EXPRESSION );
+                model.getProperty( viewpointMethod, "expression");
         EmsScriptNode viewpointExpr = null;
         if ( !Utils.isNullOrEmpty( viewpointExprs ) ) {
             viewpointExpr = viewpointExprs.iterator().next();
