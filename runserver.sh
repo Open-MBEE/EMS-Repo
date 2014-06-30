22270bdf09d66a02f2aac64aa8f3188cb3daa969
shatkhin <shatkhin@jpl.nasa.gov>
2014-06-30 16:38:01 -0700

    trying to run the script in post-build jenkins. Maven lifecycle is annoying and not intuitive

--- runserver.sh
+++ runserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cp runserver.log runserver.last.log
-mvn -e exec:exec -Pamp-to-war -Pmbee-dev -Dexec.executable="./diff2.sh" -Dexec.workingdir="./test-data/javawebscripts" -Dmaven.test.skip=true -Drebel.log=true 2>runserver.err 2>&1 | tee runserver.log
+mvn integration-test -Pamp-to-war -Pmbee-dev -Dmaven.test.skip=true -Drebel.log=true 2>runserver.err 2>&1 | tee runserver.log
 
 
 # -Dexec.workingdir="/test-data/javawebscripts" -Dexec.args="+x diff2.sh"
