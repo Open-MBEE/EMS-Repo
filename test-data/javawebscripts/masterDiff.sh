1cea48c900d008d15d4ee53c01410731e08f1ecd
shatkhin <shatkhin@jpl.nasa.gov>
2014-07-07 10:16:08 -0700

    changed up masterDiff to make more robust

--- test-data/javawebscripts/masterDiff.sh
+++ test-data/javawebscripts/masterDiff.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 #start up the server
-cd ./../..
-./runserver.sh &
+#cd ./../..
+#./runserver.sh &
+./../../runserver.sh &
 echo 'STARTING UP SERVER'
 sleep 10s
 
 #poll to see if the server is up
 cd ./test-data/javawebscripts
 server=0;
+serverCount=0;
 echo 'POLLING SERVER'
 while [ $server -eq 0 ]; do
 	netstat -ln | grep '8080' > tempMasterDiff
@@ -16,10 +18,23 @@ while [ $server -eq 0 ]; do
 	if [ $count -gt 0 ]; then
 		server=1;
 	fi
+	
+	#time-out condition
+	serverCount = serverCount + 1;
+	if [$serverCount > 10000];then
+		server=2;
+	fi
 done
-echo 'SERVER CONNECTED'
-sleep 10s
 
-#run the diff script
-echo 'RUNNING DIFF SCRIPT'
-./diff2.sh
+if [$server -eq 1]; then
+	echo 'SERVER CONNECTED'
+	sleep 10s
+
+	#run the diff script
+	echo 'RUNNING DIFF SCRIPT'
+	./diff2.sh
+
+fi
+if [$server -eq 2]; then
+	echo 'SERVER TIME-OUT'
+fi
