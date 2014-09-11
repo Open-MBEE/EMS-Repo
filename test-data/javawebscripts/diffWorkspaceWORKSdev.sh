7b7052d9b126dd435270cca12e1e346504e0a0db
gcgandhi <gorang.c.gandhi@jpl.nasa.gov>
2014-09-11 13:25:13 -0700

    Updates to diff script to use JSON diff

--- test-data/javawebscripts/diffWorkspaceWORKSdev.sh
+++ test-data/javawebscripts/diffWorkspaceWORKSdev.sh
@@ -72,7 +72,7 @@ echo 'testGET8'
 # get product list
 echo curl $CURL_FLAGS $CURL_GET_FLAGS $SERVICE_URL"ve/documents/europa?format=json\""
 curl $CURL_FLAGS $CURL_GET_FLAGS $SERVICE_URL"ve/documents/europa?format=json" | grep -v '"read":'| grep -v '"lastModified"' | grep -v '"sysmlid"' > outputWorkspaces/get8.json
-DIFF=$(diff baselineWorkspaces/get8.json outputWorkspaces/get8.json | grep -ve '---' | egrep -v "[0-9]+[c|a|d][0-9]+")
+DIFF=`java -cp .:../../src/main/amp/web/WEB-INF/lib/mbee_util.jar:../../target/view-repo-war/WEB-INF/lib/json-20090211.jar:../../target/classes gov.nasa.jpl.view_repo.util.JsonDiff baselineWorkspaces/get8.json outputWorkspaces/get8.json  | grep -v '"sysmlid"' | grep -v '"author"'| grep -v '}' | grep -ve '---' | egrep -v "[0-9]+[c|a|d][0-9]+" | grep -v '"modified":' | grep -v '"qualifiedId"'`
 if [ "$DIFF" != "" ];then
         failedTest=1
         echo "$DIFF"
