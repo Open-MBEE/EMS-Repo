2688c6b16d83e3448b81607746ecdabf34c40b5b
cinyoung <cinyoung@jpl.nasa.gov>
2014-09-16 13:38:08 -0700

    view-repo is posted to release so scripts can pull down latest that isn't snapshotted with timestamp.

--- bin/get_latest_amps.sh
+++ bin/get_latest_amps.sh
@@ -15,13 +15,13 @@ fi
 echo "path: $path"
 
 echo "determining latest version for view-share"
-export share_latest=`curl -s http://europambee-build:8082/artifactory/libs-snapshot-local/gov/nasa/jpl/view-share/maven-metadata.xml | grep latest | sed 's/<latest>//g' | sed 's/<\/latest>//g' | sed 's/ //g'`
+export share_latest=`curl -s http://europambee-build:8082/artifactory/libs-release-local/gov/nasa/jpl/view-share/maven-metadata.xml | grep latest | sed 's/<latest>//g' | sed 's/<\/latest>//g' | sed 's/ //g'`
 echo "downloading view-share-$share_latest.amp..."
 
 if [ -a $path/view-share-$share_latest.amp ]; then
   echo "view-share-$share_latest.amp already exists"
 else
-  curl -s http://europambee-build:8082/artifactory/libs-snapshot-local/gov/nasa/jpl/view-share/$share_latest/view-share-$share_latest.amp > $path/view-share-$share_latest.amp
+  curl -s http://europambee-build:8082/artifactory/libs-release-local/gov/nasa/jpl/view-share/$share_latest/view-share-$share_latest.amp > $path/view-share-$share_latest.amp
   if [ -n $owner ]; then
     chown $owner $path/view-share-$share_latest.amp
   fi
@@ -29,13 +29,13 @@ else
 fi
 
 echo "determining latest version for view-repo"
-export repo_latest=`curl -s http://europambee-build:8082/artifactory/libs-snapshot-local/gov/nasa/jpl/view-repo/maven-metadata.xml | grep latest | sed 's/<latest>//g' | sed 's/<\/latest>//g' | sed 's/ //g'`
+export repo_latest=`curl -s http://europambee-build:8082/artifactory/libs-release-local/gov/nasa/jpl/view-repo/maven-metadata.xml | grep latest | sed 's/<latest>//g' | sed 's/<\/latest>//g' | sed 's/ //g'`
 echo "downloading view-repo-$repo_latest.amp..."
 
 if [ -a $path/view-repo-$repo_latest.amp ]; then
   echo "view-repo-$repo_latest.amp already exists"
 else
-  curl -s http://europambee-build:8082/artifactory/libs-snapshot-local/gov/nasa/jpl/view-repo/$repo_latest/view-repo-$repo_latest.amp > $path/view-repo-$repo_latest.amp
+  curl -s http://europambee-build:8082/artifactory/libs-release-local/gov/nasa/jpl/view-repo/$repo_latest/view-repo-$repo_latest.amp > $path/view-repo-$repo_latest.amp
   if [ -n $owner ]; then
     chown $owner $path/view-repo-$repo_latest.amp
   fi
