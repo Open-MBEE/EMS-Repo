b84f0d7fb01c1a691654f30f5b75b0b639cfa4a3
gcgandhi <gorang.c.gandhi@jpl.nasa.gov>
2014-09-17 16:59:58 -0700

    More updates to regression script to run tests in order specified via -t or -n args.  Added -n arg to specify test based on test name.

--- test-data/javawebscripts/regression_test_harness.py
+++ test-data/javawebscripts/regression_test_harness.py
@@ -3,12 +3,10 @@
 #    -Fix run server if we use it
 #    -Get SOAP UI tests working
 #
-#    -Ability to run tests in a specified order
-#
-# BUGS:
+# BUGS (filed defects for these on JIRA):
 #    -Test 14 returning 500 if doing more than once.  This is bug w/ the lock file.
 #     Test 16 returns 404 after doing it more than once, and created time changes.  This is also a bug
-#     Test 11 return nothing if done right after posting the elements.  Doesnt work untill some delay.
+
 
 import os
 import commands
@@ -40,6 +38,7 @@ baseline_dir = ""
 display_width = 100
 test_dir_path = "test-data/javawebscripts"
 test_nums = []
+test_names = []
 create_baselines = False
 common_filters = ['"read"','"lastModified"','"modified"']
 cmd_git_branch = None
@@ -54,27 +53,35 @@ def create_command_line_options():
     An optparse.OptionParser object for parsing the command line arguments fed to this application'''
     
     usageText = '''
-    python regression_test_harness.py [-t <TESTNUMS> -b -g <GITBRANCH>]
+    python regression_test_harness.py [-t <TESTNUMS> -n <TESTNAMES> -b -g <GITBRANCH>]
     
     To run all tests for the branch:
     
-    python regression_test_harness.py -t 1,2,5-9,11
+    python regression_test_harness.py -t 1,2,11,5-9
 
-    To run test numbers 1,2,5-9,11 (overrides tests for that branch):
+    To run test numbers 1,2,11,5-9 (overrides tests for that branch):
     
     python regression_test_harness.py -t 1,2,5-9,11
     
     To create baselines of all tests for the branch:
     
     python regression_test_harness.py -b
+    
+    To run tests with names "test1" and "test2"
+    
+    python regression_test_harness.py -n test1,test2
      
     After generating the baselines you will need to copy them from testBaselineDir 
     into the desired folder for that branch, ie workspacesBaselineDir, if you like the results.  
-    This is b/c when running this outside of jenkins, the script will output to testBaselineDir.
-    If you dont like this, then change the branch name used using the -g command line arg.
+    This is b/c when running this script outside of jenkins, the script will output to testBaselineDir.
+    Alternatively, change the branch name used using the -g command line arg when creating the baselines,
+    to output to the correct baseline folder.
     
     When all tests are ran, it is all the tests for the particular branch, which is specified in
     the tests table.
+    
+    The -t option can take test numbers in any order, and it will run them in the order specified.
+    Similarly for -n option.
     '''
                 
     versionText = 'Version 1 (9_16_2014)'
@@ -82,7 +89,9 @@ def create_command_line_options():
     parser = optparse.OptionParser(usage=usageText,version=versionText)
         
     parser.add_option("-t","--testNums",action="callback",type="string",metavar="TESTNUMS",callback=parse_test_nums,
-                      help='''Specifiy the tests to run or create baselines for.  Otherwise does all tests. ie "1,2,5-9,11"  (Optional)''')
+                      help='''Specify the test numbers to run or create baselines for, ie "1,2,5-9,11".  Can only supply this if not supplying -n also.  (Optional)''')
+    parser.add_option("-n","--testNames",action="callback",type="string",metavar="TESTNAMES",callback=parse_test_names,
+                      help='''Specify the test names to run or create baselines for, ie "test1,test2".  Can only supply this if not supplying -t also.  (Optional)''')
     parser.add_option("-b","--createBaselines",action="store_true",dest="create_baselines",
                       help='''Supply this option if you want to create the baseline files for the tests (Optional)''')
     parser.add_option("-g","--gitBranch",action="callback",type="string",metavar="GITBRANCH",callback=parse_git_branch,
@@ -93,18 +102,24 @@ def create_command_line_options():
 def parse_command_line():
     '''Parse the command line options given to this application'''
 
-    global test_nums, create_baselines, cmd_git_branch
+    global test_nums, create_baselines, cmd_git_branch, test_names
     
     parser = create_command_line_options()
     
     parser.test_nums = None
     parser.cmd_git_branch = None
+    parser.test_names = None
     
     (_options,_args) = parser.parse_args()
     
     test_nums = parser.test_nums
+    test_names = parser.test_names
     create_baselines = _options.create_baselines
     cmd_git_branch = parser.cmd_git_branch
+    
+    if test_nums and test_names:
+        print "ERROR: Cannot supply both the -t and -n options!  Please remove one of them."
+        sys.exit(1)
  
 def parse_git_branch(option, opt, value, parser):
     '''
@@ -121,6 +136,20 @@ def parse_test_nums(option, opt, value, parser):
     create [1,3,4,5,7].  Assigns this to list to parser.test_nums
     '''
     
+    def parse_range(str):
+    
+        myList = []
+        keyListBounds = str.split('-')
+        bound1 = int(keyListBounds[0])
+        bound2 = int(keyListBounds[1])
+        
+        mult = 1 if bound2 >= bound1 else -1
+
+        for key in range(bound1,bound2+mult,mult):
+            myList.append(key)
+            
+        return myList
+            
     keyList = []
     
     if value is not None:
@@ -134,17 +163,7 @@ def parse_test_nums(option, opt, value, parser):
                                           
                 # testKey is a range ie 1-5:
                 if keyStr.find('-') != -1:
-                    keyListBounds = keyStr.split('-')
-                    lowerBound = int(keyListBounds[0])
-                    upperBound = int(keyListBounds[1])
-        
-                    if lowerBound > upperBound:
-                        print ('The testKey range ' + value + ' has a lower bound greater than the upper bound')
-                        print "Abadoning the script!"
-                        sys.exit(1)
-                    
-                    for key in range(lowerBound,upperBound+1):
-                        keyList.append(key)
+                    keyList += parse_range(keyStr)
                     
                 # It was a single key:
                 else:
@@ -152,22 +171,33 @@ def parse_test_nums(option, opt, value, parser):
                     
         # value is just a range ie 1-3:
         elif value.find('-') != -1:
-            keyListBounds = value.split('-')
-            lowerBound = int(keyListBounds[0])
-            upperBound = int(keyListBounds[1])
-        
-            if lowerBound > upperBound:
-                print ('The testKey range ' + value + ' has a lower bound greater than the upper bound')
-                print "Abadoning the script!"
-                sys.exit(1)
-            
-            keyList = range(lowerBound,upperBound+1)
-            
+            keyList += parse_range(keyStr)
+
         #value was just a single key:
         else:
             keyList = [int(value)]
                     
     parser.test_nums = keyList
+    
+def parse_test_names(option, opt, value, parser):
+    '''
+    Parses out the test names from the command line arg.  Assigns this to list to parser.test_names
+    '''
+    
+    keyList = []
+    
+    if value is not None:
+        value = value.strip()
+        
+        # value is comma separated:
+        if value.find(',') != -1:
+            keyList = value.split(',')
+                       
+        #value was just a single key:
+        else:
+            keyList = [value]
+                    
+    parser.test_names = keyList
 
 def thick_divider():
     print "\n"+"="*display_width+"\n"
@@ -193,13 +223,14 @@ def mbee_util_jar_path():
     else:
         return path+"0.0.16/mbee_util-0.0.16.jar"
 
-def run_curl_test(test_num, test_desc, curl_cmd, use_json_diff=False, filters=None,
+def run_curl_test(test_num, test_name, test_desc, curl_cmd, use_json_diff=False, filters=None,
                   delay=None):
     '''
     Runs the curl test and diffs against the baseline if create_baselines is false, otherwise
     runs the curl command and creates the baseline .json file. 
     
     test_num: The unique test number for this test
+    test_name: The name of the test
     test_desc: The test description
     curl_cmd: The curl command to send
     use_json_diff: Set to True to use a JsonDiff when comparing to the baseline
@@ -214,11 +245,11 @@ def run_curl_test(test_num, test_desc, curl_cmd, use_json_diff=False, filters=No
 
     thick_divider()
     if create_baselines:
-        print "CREATING BASELINE FOR TEST NUMBER "+str(test_num)
+        print "CREATING BASELINE FOR TEST %s (%s)"%(test_num, test_name)
         orig_json = baseline_orig_json
         filtered_json = baseline_json
     else:
-        print "TEST NUMBER "+str(test_num)
+        print "TEST %s (%s)"%(test_num, test_name)
         orig_json = result_orig_json
         filtered_json = result_json
         
@@ -353,31 +384,33 @@ def startup_server():
         print "SERVER CONNECTED"
         
     else:
-        print "SERVER TIME-OUT"
+        print "ERROR: SERVER TIME-OUT"
         kill_server()
-        exit(1)
+        sys.exit(1)
         
         
 ##########################################################################################
 #
 # TABLE OF ALL POSSIBLE TESTS TO RUN
-#     TEST MUST BE ADDED HERE WITH A UNIQUE TEST NUMBER
+#     MUST HAVE A UNIQUE TEST NUMBER AND UNIQUE TEST NAME
 #
 tests =[\
         
 # [       
 # Test Number, 
+# Test Name,
 # Test Description,
 # Curl Cmd, 
 # Use JsonDiff, 
 # Output Filters (ie lines in the .json output with these strings will be filtered out)
 # Branch Names that will run this test by default
-# Delay before running the test (Optional)
+# Delay in seconds before running the test (Optional)
 # ]
 
 # POSTS: ==========================
 [
-1, 
+1,
+"PostSite",
 "Create a project and site",
 create_curl_cmd(type="POST",data='\'{"name":"JW_TEST"}\'',base_url=BASE_URL_JW,
                 branch="sites/europa/projects/123456",project_post=True),
@@ -387,7 +420,8 @@ None,
 ],
  
 [
-2, 
+2,
+"PostElementsNew",
 "Post elements to the master branch",
 create_curl_cmd(type="POST",data="elementsNew.json",base_url=BASE_URL_WS,
                 post_type="elements",branch="master/"),
@@ -398,6 +432,7 @@ common_filters,
         
 [
 3,
+"PostViews",
 "Post views",
 create_curl_cmd(type="POST",data="views.json",base_url=BASE_URL_JW,
                 post_type="views",branch=""),
@@ -407,7 +442,8 @@ None,
 ],
         
 [
-4, 
+4,
+"PostProducts",
 "Post products",
 create_curl_cmd(type="POST",data="products.json",base_url=BASE_URL_JW,
                 post_type="products",branch=""),
@@ -418,7 +454,8 @@ None,
   
 # GETS: ==========================    
 [
-5, 
+5,
+"GetProject",
 "Get project",
 create_curl_cmd(type="GET",data="sites/europa/projects/123456",base_url=BASE_URL_JW,
                 branch=""),
@@ -428,7 +465,8 @@ None,
 ],
         
 [
-6, 
+6,
+"GetElements",
 "Get elements",
 create_curl_cmd(type="GET",data="elements/123456?recurse=true",base_url=BASE_URL_JW,
                 branch=""),
@@ -438,7 +476,8 @@ common_filters,
 ],
         
 [
-7, 
+7,
+"GetViews",
 "Get views",
 create_curl_cmd(type="GET",data="views/301",base_url=BASE_URL_JW,
                 branch=""),
@@ -448,7 +487,8 @@ common_filters,
 ],
         
 [
-8, 
+8,
+"GetViewElements",
 "Get view elements",
 create_curl_cmd(type="GET",data="views/301/elements",base_url=BASE_URL_JW,
                 branch=""),
@@ -458,7 +498,8 @@ common_filters,
 ],
      
 [
-9, 
+9,
+"GetProducts",
 "Get product",
 create_curl_cmd(type="GET",data="products/301",base_url=BASE_URL_JW,
                 branch=""),
@@ -468,7 +509,8 @@ common_filters,
 ],
                    
 [
-10, 
+10,
+"GetProductList",
 "Get product list",
 create_curl_cmd(type="GET",data="ve/documents/europa?format=json",base_url=SERVICE_URL,
                 branch=""),
@@ -476,9 +518,12 @@ True,
 common_filters,
 ["test","workspaces","develop"]
 ],
-        
+   
+# Note: Need a delay before doing this search, b/c it will find "some*" within the
+#       documentation, which does not get indexed by alfresco right away
 [
-11, 
+11,
+"GetSearch",
 "Get search",
 create_curl_cmd(type="GET",data="",base_url=BASE_URL_JW,
                 branch="element/search?keyword=some*"),
@@ -491,7 +536,8 @@ common_filters,
 # DELETES: ==========================    
       
 [
-12, 
+12,
+"Delete6666",
 "Delete element 6666",
 create_curl_cmd(type="DELETE",data="elements/6666",base_url=BASE_URL_WS,
                 branch="master/"),
@@ -503,7 +549,8 @@ common_filters+['"timestamp"','"sysmlid"','"id"','"qualifiedId"','"version"'],
 # POST CHANGES: ==========================    
 
 [
-13, 
+13,
+"PostChange",
 "Post changes to directed relationships only (without owners)",
 create_curl_cmd(type="POST",data="directedrelationships.json",base_url=BASE_URL_JW,
                 branch="sites/europa/projects/123456/",post_type="elements"),
@@ -515,7 +562,8 @@ common_filters,
 # CONFIGURATIONS: ==========================    
 
 [
-14, 
+14,
+"PostConfig",
 "Post configuration",
 create_curl_cmd(type="POST",data="configuration.json",base_url=BASE_URL_JW,
                 branch="configurations/europa",post_type=""),
@@ -525,7 +573,8 @@ common_filters+['"timestamp"','"id"'],
 ],
         
 [
-15, 
+15,
+"GetConfig",
 "Get configurations",
 create_curl_cmd(type="GET",data="configurations/europa",base_url=BASE_URL_JW,
                 branch=""),
@@ -537,7 +586,8 @@ common_filters+['"timestamp"','"id"'],
 # WORKSPACES: ==========================    
 
 [
-16, 
+16,
+"CreateWorkspace1",
 "Create workspace test 1",
 create_curl_cmd(type="POST",base_url=BASE_URL_WS,
                 post_type="",branch="wsA?sourceWorkspace=master"),
@@ -547,7 +597,8 @@ common_filters+['"branched"','"created"'],
 ],
         
 [
-17, 
+17,
+"CreateWorkspace2",
 "Create workspace test 2",
 create_curl_cmd(type="POST",base_url=BASE_URL_WS,
                 post_type="",branch="wsB?sourceWorkspace=wsA"),
@@ -558,6 +609,7 @@ common_filters+['"branched"','"created"'],
         
 [
 18, 
+"GetWorkspaces",
 "Get workspaces",
 create_curl_cmd(type="GET",base_url=BASE_URL_WS_NOBS,branch=""),
 True, 
@@ -567,8 +619,18 @@ common_filters+['"branched"','"created"'],
         
 # SNAPSHOTS: ==========================    
 
-# TODO
-
+[
+19,
+"PostSnapshot",
+"Post snapshot test",
+create_curl_cmd(type="POST",base_url=BASE_URL_WS,
+                branch="master/sites/europa/products/301/",
+                post_type="snapshots"),
+True, 
+common_filters+['"created"','"id"','"url"'],
+["test","workspaces","develop"]
+],
+                
 ]    
 
 ##########################################################################################    
@@ -586,7 +648,7 @@ if __name__ == '__main__':
     # Change directories to where we are used to sending curl cmds:
     if not os.path.exists(test_dir_path):
         print "ERROR: Test directory path '%s' does not exists!\n"%test_dir_path
-        exit(1)
+        sys.exit(1)
     
     os.chdir(test_dir_path)
 
@@ -613,28 +675,36 @@ if __name__ == '__main__':
     print "\nUSING BASELINE DIR: '%s'\nOUTPUT DIR: '%s'\n"%(baseline_dir, result_dir)
     
     # Run tests or create baselines:
-    for test in tests:
-        test_num = test[0]
-        
-        # If the test number is in the list of ones to run:
-        if test_nums:
-            if test_num in test_nums:
-                run_curl_test(test_num=test_num,
-                              test_desc=test[1],
-                              curl_cmd=test[2],
-                              use_json_diff=test[3],
-                              filters=test[4],
-                              delay=test[6] if (len(test) > 6) else None)
-                
-        # Otherwise if the test should be run for the current branch:
-        else:
-            if git_branch in test[5]:
-                run_curl_test(test_num=test_num,
-                              test_desc=test[1],
-                              curl_cmd=test[2],
-                              use_json_diff=test[3],
-                              filters=test[4],
-                              delay=test[6] if (len(test) > 6) else None)
+    # If there were test numbers specified:
+    if test_nums:
+        for test_num in test_nums:
+            # If it is a valid test number then run the test:
+            for test in tests:
+                if test_num == test[0]:
+                     run_curl_test(test_num=test[0],test_name=test[1],
+                                  test_desc=test[2],curl_cmd=test[3],
+                                  use_json_diff=test[4],filters=test[5],
+                                  delay=test[7] if (len(test) > 7) else None)
+                     
+    # If there were test names specified:
+    elif test_names:
+        for test_name in test_names:
+                # If it is a valid test number then run the test:
+                for test in tests:
+                    if test_name == test[1]:
+                         run_curl_test(test_num=test[0],test_name=test[1],
+                                      test_desc=test[2],curl_cmd=test[3],
+                                      use_json_diff=test[4],filters=test[5],
+                                      delay=test[7] if (len(test) > 7) else None)
+                         
+    # Otherwise, run all the tests for the branch:
+    else:
+        for test in tests:            
+            if git_branch in test[6]:
+                    run_curl_test(test_num=test[0],test_name=test[1],
+                                  test_desc=test[2],curl_cmd=test[3],
+                                  use_json_diff=test[4],filters=test[5],
+                                  delay=test[7] if (len(test) > 7) else None)
                 
     # uncomment once startup_server() works
 #     print "KILLING SERVER"
@@ -644,7 +714,7 @@ if __name__ == '__main__':
         print "\nNUMBER OF PASSED TESTS: "+str(passed_tests)
         print "NUMBER OF FAILED TESTS: "+str(failed_tests)+"\n"
     
-    exit(failed_tests)
+    sys.exit(failed_tests)
     
     
     
