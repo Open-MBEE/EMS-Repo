3c5c4a2d84e5c472a0c45fb8b8160bb67309ab46
lho <lho@jpl.nasa.gov>
2014-10-06 10:36:09 -0700

    CMED-378: docbook conversion bugs for html fixed section ordering fixed table rowspan and colspan.

---
+++ src/main/java/gov/nasa/jpl/view_repo/webscripts/DocBookTable.java
@@ -0,0 +1,58 @@
+package gov.nasa.jpl.view_repo.webscripts;
+
+public class DocBookTable {
+	private int colCount;
+	private int headerRowCount;
+	private int bodyRowCount;
+	private int header[][];
+	private int body[][];
+	
+	public int[][] getBody(){return body;}
+	
+	public int getBodyRowCount(){return bodyRowCount;}
+	
+	public int getColCount(){ return colCount;}
+	
+	public int[][] getHeader(){return header;}
+
+	public int getHeaderRowCount(){return headerRowCount;}
+
+	public int getStartCol(int row, int col, int rowspan, int colspan, boolean isHeader){
+		int startCol = 0;
+		boolean isSet = false;
+		for(int i = row; i < row + rowspan; i++){
+			for(int j = col; j < col+colspan; j++){
+				if(isHeader){
+					if(i < headerRowCount && j < colCount){
+						if(header[i][j] == 0 && !isSet){
+							startCol = j+1;
+							isSet = true;
+						}
+						header[i][j] = 1;
+					}
+				}
+				else{
+					if(i < bodyRowCount && j < colCount)
+						if(body[i][j] == 0 && !isSet){
+							startCol = j+1;
+							isSet = true;
+						}
+						body[i][j] = 1;
+				}
+			}
+		}
+		return startCol;
+	}
+	
+	public void init(){
+		header = new int[headerRowCount][colCount];
+		body = new int[bodyRowCount][colCount];
+	}
+	
+	public void setBodyRowCount(int rowCount){bodyRowCount = rowCount;}
+
+	public void setColCount(int colCount){this.colCount = colCount;}
+	
+	public void setHeaderRowCount(int rowCount){headerRowCount = rowCount;}
+	
+}
