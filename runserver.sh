3daf6a5db0350c3c379dd4a45687753e3609e761
bclement <bclement@jpl.nasa.gov>
2015-01-30 18:21:22 -0800

    added pom for cinyoug/alfresco engineering fix to Version2ServiceImpl

--- runserver.sh
+++ runserver.sh
@@ -9,8 +9,18 @@ if [ -f runserver.out ]; then
   cp runserver.out runserver.last.out
 fi
 
+echo "arg 1 = $1"
+
+if [ "$1" == "" ]; then
+  pom=pom.xml
+else
+  pom=$1
+fi
 
 # DO NOT CHANGE TO -P OPTION AS JENKINS NEEDS IT THIS WAY!
 
-mvn integration-test -U -Pmbee-dev -Pamp-to-war -Dmaven.test.skip=true -Drebel.log=true 2>runserver.err 2>&1 | tee runserver.log | tee runserver.out
+#echo "mvn integration-test -U -f $pom -Pmbee-dev -Pamp-to-war -Dmaven.test.skip=true -Drebel.log=true 2>runserver.err 2>&1 | tee runserver.log | tee runserver.out"
+#mvn integration-test -U -f $pom -Pmbee-dev -Pamp-to-war -Dmaven.test.skip=true -Drebel.log=true 2>runserver.err 2>&1 | tee runserver.log | tee runserver.out
+echo "mvn integration-test -U -f $pom -Pmbee-dev -Pamp-to-war -Dmaven.test.skip=true 2>runserver.err 2>&1 | tee runserver.log | tee runserver.out"
+mvn integration-test -U -f $pom -Pmbee-dev -Pamp-to-war -Dmaven.test.skip=true 2>runserver.err 2>&1 | tee runserver.log | tee runserver.out
 
