c9ac2b12fdf06cea609f6f683f6f2127bb618c7f
bclement <bclement@jpl.nasa.gov>
2015-03-27 10:09:12 -0700

    Removing constructors from List that may cause problems; updating baseline for Demo1, which was somehow returning two elements instead of one, and reindenting BluCamNameListExpr.json.

--- src/main/java/gov/nasa/jpl/view_repo/sysml/List.java
+++ src/main/java/gov/nasa/jpl/view_repo/sysml/List.java
@@ -48,54 +48,54 @@ public class List extends ArrayList< Viewable< EmsScriptNode > > implements sysm
         super( c );
     }
 
-    public < V extends Viewable<EmsScriptNode > > List( String viewOperation,
-                                                        Collection< EmsScriptNode > c ) throws ClassNotFoundException {
-        this( ClassUtils.getClassForName( viewOperation, "toViewJson",
-                                          List.class.getPackage().getName(), false ),
-              c );//classForName( viewOperation ), c );
-    }
-    public < V extends Viewable<EmsScriptNode > > List( Class< V > viewOperation,
-                                                        Collection< EmsScriptNode > c ) {
-        this();
-        if ( Utils.isNullOrEmpty( c ) ) return;
-
-        if ( viewOperation == null ) {
-            viewOperation = (Class< V >)Name.class;
-        }
-        @SuppressWarnings( "unchecked" )
-        Constructor< V > ctor = 
-                (Constructor< V >)ClassUtils.getConstructorForArgs( viewOperation,
-                                          new Object[] {c.iterator().next()} );
-        for (EmsScriptNode obj : c) {
-            if ( obj == null ) continue;  // REVIEW -- add null instead?
-            try {
-                this.add( ctor.newInstance( new Object[]{c} ) );
-            } catch ( InstantiationException e ) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch ( IllegalAccessException e ) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch ( IllegalArgumentException e ) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            } catch ( InvocationTargetException e ) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public < V extends Viewable<EmsScriptNode > >List(String viewOperation,
-                                                      EmsScriptNode... c) {
-        this( ClassUtils.getClassForName( viewOperation, "toViewJson",
-                                          List.class.getPackage().getName(), false ),
-              Utils.arrayAsList(c) );
-    }
-    public < V extends Viewable<EmsScriptNode > >List(Class< V > viewOperation,
-                                                      EmsScriptNode... c) {
-        this( viewOperation, Utils.arrayAsList(c) );
-    }
+//    public < V extends Viewable<EmsScriptNode > > List( String viewOperation,
+//                                                        Collection< EmsScriptNode > c ) throws ClassNotFoundException {
+//        this( ClassUtils.getClassForName( viewOperation, "toViewJson",
+//                                          List.class.getPackage().getName(), false ),
+//              c );//classForName( viewOperation ), c );
+//    }
+//    public < V extends Viewable<EmsScriptNode > > List( Class< V > viewOperation,
+//                                                        Collection< EmsScriptNode > c ) {
+//        this();
+//        if ( Utils.isNullOrEmpty( c ) ) return;
+//
+//        if ( viewOperation == null ) {
+//            viewOperation = (Class< V >)Name.class;
+//        }
+//        @SuppressWarnings( "unchecked" )
+//        Constructor< V > ctor = 
+//                (Constructor< V >)ClassUtils.getConstructorForArgs( viewOperation,
+//                                          new Object[] {c.iterator().next()} );
+//        for (EmsScriptNode obj : c) {
+//            if ( obj == null ) continue;  // REVIEW -- add null instead?
+//            try {
+//                this.add( ctor.newInstance( new Object[]{c} ) );
+//            } catch ( InstantiationException e ) {
+//                // TODO Auto-generated catch block
+//                e.printStackTrace();
+//            } catch ( IllegalAccessException e ) {
+//                // TODO Auto-generated catch block
+//                e.printStackTrace();
+//            } catch ( IllegalArgumentException e ) {
+//                // TODO Auto-generated catch block
+//                e.printStackTrace();
+//            } catch ( InvocationTargetException e ) {
+//                // TODO Auto-generated catch block
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+//
+//    public < V extends Viewable<EmsScriptNode > >List(String viewOperation,
+//                                                      EmsScriptNode... c) {
+//        this( ClassUtils.getClassForName( viewOperation, "toViewJson",
+//                                          List.class.getPackage().getName(), false ),
+//              Utils.arrayAsList(c) );
+//    }
+//    public < V extends Viewable<EmsScriptNode > >List(Class< V > viewOperation,
+//                                                      EmsScriptNode... c) {
+//        this( viewOperation, Utils.arrayAsList(c) );
+//    }
     
     // FIXME java reflection is not smart enough to handle this correctly, 
     //		 as it doesnt work with an array of Names in a Object array, even
