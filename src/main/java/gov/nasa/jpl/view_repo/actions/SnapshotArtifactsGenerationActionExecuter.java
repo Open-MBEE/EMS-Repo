e12430110edc7c28517dd47f24c8abfd765d86ca
Ly C Ho <lho@jpl.nasa.gov>
2015-04-08 13:23:22 -0700

    filtered out transclussion comment tags

--- src/main/java/gov/nasa/jpl/view_repo/actions/SnapshotArtifactsGenerationActionExecuter.java
+++ src/main/java/gov/nasa/jpl/view_repo/actions/SnapshotArtifactsGenerationActionExecuter.java
@@ -235,7 +235,7 @@ public class SnapshotArtifactsGenerationActionExecuter  extends ActionExecuterAb
         	logger.error(sb.toString());
         	ex.printStackTrace();
         	ActionUtil.sendEmailToModifier(jobNode, String.format("An unexpected error occurred and your PDF generation failed.\n%s%s", ex.getMessage(), sb.toString()), "PDF Generation Failed", services);
-        	ActionUtil.sendEmailTo("lho@jpl.nasa.gov", "lho@jpl.nasa.gov", 
+        	ActionUtil.sendEmailTo("mbee-dev-admin@jpl.nasa.gov", "mbee-dev-admin@jpl.nasa.gov", 
         			String.format("Server: %s\nSite: %s\nWorkspace: %s\nSnapshot Id: %s\nError: %s%s%s", 
         					new HostnameGet(this.repository, this.services).getAlfrescoUrl(),
         					siteName,
