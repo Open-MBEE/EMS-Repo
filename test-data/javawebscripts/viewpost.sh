1eb498a94322ca2a8598c4b250ac77ea283d6742
bclement <bclement@jpl.nasa.gov>
2015-04-09 16:08:10 -0700

    changing ports back to 8080 and 10000

--- test-data/javawebscripts/viewpost.sh
+++ test-data/javawebscripts/viewpost.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -w "%{http_code}\n" -X POST -u admin:admin -H "Content-Type:application/json" --data @$1 "http://localhost:9093/view-repo/service/javawebscripts/views"
+curl -w "%{http_code}\n" -X POST -u admin:admin -H "Content-Type:application/json" --data @$1 "http://localhost:8080/view-repo/service/javawebscripts/views"
