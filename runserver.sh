cb54ed74bd6fe072171f290d876e5a4525e1685d
Bradley J. Clement <bclement@ljpl.nasa.gov>
2015-07-10 17:15:11 -0700

    accessing Klang project from artifactory; switching back to mbee profile

--- runserver.sh
+++ runserver.sh
@@ -19,8 +19,8 @@ fi
 
 # DO NOT CHANGE TO -P OPTION AS JENKINS NEEDS IT THIS WAY!
 
-#echo "mvn integration-test -U -f $pom -Pmms-dev -Pamp-to-war -Dmaven.test.skip=true -Drebel.log=true 2>runserver.err 2>&1 | tee runserver.log | tee runserver.out"
-#mvn integration-test -U -f $pom -Pmms-dev -Pamp-to-war -Dmaven.test.skip=true -Drebel.log=true 2>runserver.err 2>&1 | tee runserver.log | tee runserver.out
-echo "mvn integration-test -U -f $pom -Pmms-dev -Pamp-to-war -Dmaven.test.skip=true 2>runserver.err 2>&1 | tee runserver.log | tee runserver.out"
-mvn integration-test -U -f $pom -Pmms-dev -Pamp-to-war -Dmaven.test.skip=true -Dmaven.tomcat.port=8080 2>runserver.err 2>&1 | tee runserver.log | tee runserver.out
+#echo "mvn integration-test -U -f $pom -Pmbee-dev -Pamp-to-war -Dmaven.test.skip=true -Drebel.log=true 2>runserver.err 2>&1 | tee runserver.log | tee runserver.out"
+#mvn integration-test -U -f $pom -Pmbee-dev -Pamp-to-war -Dmaven.test.skip=true -Drebel.log=true 2>runserver.err 2>&1 | tee runserver.log | tee runserver.out
+echo "mvn integration-test -U -f $pom -Pmbee-dev -Pamp-to-war -Dmaven.test.skip=true 2>runserver.err 2>&1 | tee runserver.log | tee runserver.out"
+mvn integration-test -U -f $pom -Pmbee-dev -Pamp-to-war -Dmaven.test.skip=true -Dmaven.tomcat.port=8080 2>runserver.err 2>&1 | tee runserver.log | tee runserver.out
 
