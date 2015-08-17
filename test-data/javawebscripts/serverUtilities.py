ad86b779cbfeeda78be5b17c60a04cb069aad2a2
erik <dan.e.karlsson@jpl.nasa.gov>
2015-08-17 12:43:13 -0700

    It will create elements. Get methods have 2 versions, one for return strings, one for returning serialized json key/value

--- test-data/javawebscripts/serverUtilities.py
+++ test-data/javawebscripts/serverUtilities.py
@@ -1,9 +1,8 @@
 from regression_test_harness import *
 import json
-import sys
 '''
 Python's JSON module usage, tips and tricks
- json.dumps(jsonObject) will take jsonObject and create a python object
+ json.dump(jsonObject) will take jsonObject and create a python object
  Differences of importing json objects
  If importing as a string, attributes and values of the json objects require to have double quotations ""
  >>> testElement = importTestViewJson('[{'name':'example'},{'name':'ex2'}]')
@@ -53,11 +52,165 @@ Python's JSON module usage, tips and tricks
 	Remove view from its parent
 	Set qualified ID
 	Set owner
+*************
+	EXAMPLE COMMAND AND RESULT
+*************
+	python -c 'from serverUtilities import *;jsonObjFile = open("./JsonData/testProductElement.json");jsonObj = json.load(jsonObjFile);print jsonObj.get("elements"); utilObj = UtilElement("./JsonData/testProductElement.json");print utilObj.getId(); viewObj = UtilElement("./JsonData/testViewElement.json");print viewObj;utilObj.addView("./testViewElement.json")'
+[{u'qualifiedName': u'/server-utils/server-utils_no_project/holding_bin/Test Doc 1', u'name': u'Test Doc 1', u'created': u'Sun Aug 16 18:26:50 PDT 2015', u'sysmlid': u'MMS_1439774810522_0ba947a4-ac47-4d7d-b17b-e0c692243e88', u'documentation': u'This element is on ems-stg under Server Utilities', u'editable': True, u'modified': u'2015-08-16T19:09:11.973-0700', u'read': u'2015-08-16T19:09:12.373-0700', u'siteCharacterizationId': u'server-utils', u'owner': u'holding_bin_server-utils_no_project', u'qualifiedId': u'/server-utils/server-utils_no_project/holding_bin_server-utils_no_project/MMS_1439774810522_0ba947a4-ac47-4d7d-b17b-e0c692243e88', u'modifier': u'example', u'creator': u'dank', u'specialization': {u'type': u'Product', u'displayedElements': [u'MMS_1439774810522_0ba947a4-ac47-4d7d-b17b-e0c692243e88'], u'allowedElements': [u'MMS_1439774810522_0ba947a4-ac47-4d7d-b17b-e0c692243e88'], u'view2view': [{u'childrenViews': [], u'id': u'MMS_1439774810522_0ba947a4-ac47-4d7d-b17b-e0c692243e88'}], u'contents': {u'operand': [{u'instance': u'MMS_1439774811887_0ecc5284-0e8a-4263-a0f1-51ba51af6ad4', u'valueExpression': None, u'type': u'InstanceValue'}], u'valueExpression': None, u'type': u'Expression'}}}]
+Loaded JSON Object
+None
+Loaded JSON Object
+<serverUtilities.UtilElement instance at 0x1018df830>
+Loaded JSON Object
+
+
+
 '''
 
-def dumpJson(testView):
+
+class UtilElement:
+    def __init__(self, inElement=None, base_url=BASE_URL_WS, workspace='master'):
+        self.data = []
+        if (inElement is not None):
+            self.jsonObj = loadJson(open(inElement))  # Function that will throw exception if it is not the correct json
+            self.element = self.jsonObj.get('elements')[0]
+            self.sysmlid = self.element.get('symlid')
+            self.owner = self.element.get('owner')
+            self.specialization = self.element.get('specialization')
+            self.type = self.specialization.get('type')
+            self.name = self.element.get('name')
+            self.read = self.element.get('read')
+            self.documentation = self.element.get('documentation')
+            self.created = self.element.get('created')
+            get = self.element.get('qualifiedId')
+            self.qualifiedId = get
+            if (self.type == 'View'):
+                self.childViews = []
+                self.childViews = self.specialization.get('childrenViews')
+
+            elif (self.type == 'Product'):
+                self.view2views = self.specialization.get('view2view')
+                index = 0
+                self.childViews = []
+                for each in self.view2views:
+                    addChild = self.view2views[index].get('childrenViews')
+                    self.childViews.append(addChild)
+                    index = index + 1
+
+            self.displayed = self.specialization.get('displayedElements')
+            self.contents = self.specialization.get('contents')
+            self.contentType = self.contents.get('type')
+            self.valueExpression = self.contents.get('valueExpression')
+            self.operand = self.contents.get('operand')
+
+        self.base_url = base_url
+        self.workspace = workspace
+
+    # ------------------------------------
+    #   JSON Get Methods
+    # ------------------------------------
+    def getId(self):
+        return self.sysmlid
+    def getOwner(self):
+        return self.owner
+    def getType(self):
+        return self.type
+    def getName(self):
+        return self.name
+    def getDocs(self):
+        return self.documentation
+    def getQualifiedId(self):
+        return self.qualifiedId
+    def getTimeCreated(self):
+        return self.created
+    def getChildViews(self):
+        return self.childViews
+
+    # ------------------------------------
+    #   String Get Methods
+    # ------------------------------------
+    def getStrId(self):
+        return str(self.sysmlid)
+    def getStrOwner(self):
+        return str(self.owner)
+    def getStrType(self):
+        return str(self.type)
+    def getStrName(self):
+        return str(self.name)
+    def getStrDocs(self):
+        return str(self.documentation)
+    def getStrQualifiedId(self):
+        return str(self.qualifiedId)
+    def getStrTimeCreated(self):
+        return str(self.created)
+    def getStrChildViews(self):
+        return str(self.childViews)
+
+    # ------------------------------------
+    #   Mutator Methods
+    # ------------------------------------
+    def addView(self,newView):
+        newViewUtil = UtilElement("./JsonData/"+newView)
+        tempView = []
+        index = 0
+        for each in newViewUtil.childViews:
+            tempView.append(newViewUtil.childViews[index])
+            index = index + 1
+        index = 0
+        for each in self.childViews:
+            tempView.append(self.childViews[index])
+            index = index + 1
+        self.childViews = tempView
+
+    # ------------------------------------
+    #   RESTful Methods
+    # ------------------------------------
+
+
+    #   POST Method
+
+    def POST(self, verbose=True):
+        if (self.jsonObj is not None):
+            curl_cmd = create_curl_cmd(type="POST", data=self.jsonObj, base_url=self.base_url, branch=self.workspace)
+        else:
+            print('No element...\nPosted nothing...\n\n')
+            return
+        # no need for time delay when posting elements or changes to elements
+        if (verbose):
+            return execCurlCmd(curl_cmd)
+
+    def GET(self):
+        if(self.sysmlid is not ''):
+            if(self.type is 'View'):
+                return getViewById(self.sysmlid)
+            elif(self.type is 'Product'):
+                return getElementById(self.sysmlid)
+            else:
+                print('Not a view or a product')
+
+
+    def evaluate(self):
+        curl_cmd = create_curl_cmd(type="GET", data=self.element,
+                                   base_url=BASE_URL_WS,
+                                   branch=self.workspace)
+
+        eval_cmd = curl_cmd + '?evalute'
+        return execCurlCmd(eval_cmd)
+
+
+    def removeOwnership(self):
+        owner = self.owner
+        curl_cmd = create_curl_cmd("GET")
+
+    # curl_cmd = create_curl_cmd("GET", data=self.jsonObj, base_url=self.baseUrl)
+    # utilElement = commands.getoutput(curl_cmd)
+
+# Utility functions to be used outside the class
+
+# Attempts to serialize the json file
+def dumpJson(jsonObject):
     try:
-        dumpJsonObj = json.dumps(testView)
+        dumpJsonObj = json.dump(jsonObject)
     except:
         print('Could not dump element...\n\nCheck json file')
         raise
@@ -65,9 +218,9 @@ def dumpJson(testView):
         print('Dumped JSON Object')
         return dumpJsonObj
 
-def loadJson(testView):
+def loadJson(jsonObject):
     try:
-        loadJsonObj = json.loads(testView)
+        loadJsonObj = json.load(jsonObject)
     except:
         print('Could not load element...\n\nCheck json file')
         raise
@@ -82,11 +235,11 @@ def sizeOf(element):
 def testElement(element):
     loadJsonObj = "{[{}]}"
     try:
-        loadJsonObj = json.loads(element)
+        loadJsonObj = json.load(open(element))
     except loadJsonObj:
         print('Could not load element...\n\nAttempting to dump...\n\n')
         try:
-            dumpJsonObj = json.dumps(element)
+            dumpJsonObj = json.dump(element)
         except:
             print('Could not dump element...\n\nCheck json file')
             raise
@@ -105,64 +258,8 @@ def getElementInJson(jsonElement=None, index=0):
             print('\n\nIndex: ' + index + ' is out of bounds!\n\nThere are: ' + len(jsonAr))
 
 
-class utilElement:
-    def __init__(self, inElement=None, base_url='', workspace='master'):
-        self.data = []
-        if (inElement is not None):
-            # try:
-            #     self.jsonObj = json.loads(inElement)
-            # except self.jsonObj:
-            #     print('Could not load element...\n\n')
-            #
-            #     raise
 
-            self.element = self.jsonObj
-
-            self.sysmlid = self.element.symlid
-            self.owner = self.element.owner
-            self.type = self.element.specialization.type
-            self.name = self.element.name
-            self.read = self.element.read
-            self.documentation = self.element.documentation
-            self.created = self.element.created
-            self.qualifiedId = self.element.qualifiedId
-            if (self.type == 'View'):
-                self.childViews = self.element.childrenViews
-            elif (self.type == 'Product'):
-                self.childViews = self.element.specialization.view2view.childrenViews
-        self.baseUrl = base_url
-        self.workspace = workspace
-
-    def removeOwnership(self):
-        curl_cmd = create_curl_cmd("GET", CURL_FLAGS)
-
-        # curl_cmd = create_curl_cmd("GET", data=self.jsonObj, base_url=self.baseUrl)
-        # utilElement = commands.getoutput(curl_cmd)
-
-
-    def post(self, entireJsonData=None, workspaceName='master/', post_type='elements', project_post=False,
-             verbose=True):
-        if(entireJsonData is not None):
-            curl_cmd = create_curl_cmd(type="POST", data=self.element, base_url=BASE_URL_WS, branch=workspaceName)
-        else:
-            print('No element...\nPosted nothing...\n\n')
-            return
-        # no need for time delay when posting elements or changes to elements
-        if (verbose):
-            execCurlCmd(curl_cmd)
-
-    def evaluate(self):
-        curl_cmd = create_curl_cmd(type="GET", data=self.element,
-                                   base_url=BASE_URL_WS,
-                                   branch=self.workspace)
-
-        eval_cmd = curl_cmd + '?evalute'
-        execCurlCmd(eval_cmd)
-
-
-# Utility functions to be used outside the class
 def evalElement(jsonFile, url=HOST, workspace='master/'):
-    url = 'https://'+ url + '.jpl'
     curl_cmd = create_curl_cmd(type="GET", data=jsonFile,
                                base_url=url,
                                branch=workspace )
@@ -173,29 +270,54 @@ def addViewToView(toAdd, addingTo):
     if (toAdd is None or addingTo is None):
         print("Views don't exists")
         return
+    # Testing basic function first then implementing class
+    # child = UtilElement(toAdd)
+    # parent = UtilElement(addingTo)
 
-    temp = json.loads(toAdd)
-    child = temp[0]
-    temp = json.loads(addingTo)
-    parent = temp[0]
+    child = UtilElement("./JsonData/"+ toAdd)
+    parent = UtilElement("./JsonData/" + addingTo)
 
-    childType = child.specialization.type
-    parentType = parent.specialization.type
+    # temp = loadJson(toAdd)
+    # child = temp[0]
+    # temp = loadJson(addingTo)
+    # parent = temp[0]
 
-    if (parentType == 'Product'):
-        for index in parent.specialization.view2view:
-            if (parent.specialization.view2view.id == parent.sysmlid):
-                if (childType == 'Product'):
+
+
+    if (parent.getStrType() == 'Product'):
+        index = 0
+        for each in parent:
+
+            if (parent.specialization.view2view[index].id == parent.sysmlid):
+                if (child.getStrtype() == 'Product'):
+                    tempView = parent.specialization.view2view[0]
+                    tempView.set
                     parent.specialization.view2view[index].childrenViews.append(child.sysmlid)
 
                 parent.specialization.view2view.append()
+            index = index + 1
+
+
 
 def getElementById(sysmlid=''):
-    (status,output) = commands.getstatusoutput('curl -w "%{http_code}\n" -X GET -u admin:admin http://localhost:8080/alfresco/service/workspaces/master/elements/sysmlid')
-    print('Status:\n'+ status + '\n')
-    print('Output:\n'+ output + '\n')
-    return output
+    getUrl = 'http://localhost:8080/alfresco/service/workspaces/master/elements/' + str(sysmlid)
+    get_cmd = 'curl -w "%{http_code}\n" -X GET -u admin:admin '
+    send_cmd = get_cmd + getUrl
+    element = execCurlCmd(send_cmd)
+    return element
+
+def getViewById(sysmlid=''):
+    getUrl = '\"http://localhost:8080/alfresco/service/rest/views/' + str(sysmlid) + '\"'
+    get_cmd = 'curl -w "%{http_code}\n" -X GET -u admin:admin '
+    send_cmd = get_cmd + getUrl
+    view = execCurlCmd(send_cmd)
+    return view
 
 def execCurlCmd(cmd):
     (status, output)= commands.getstatusoutput(cmd)
-    print('Status: ' + status + '\n' + "Output: " + output)
+    return status, output
+
+def testCreateUtilObject():
+    utilObject = UtilElement("./JsonData/testProductElement.json")
+    print utilObject
+    print utilObject
