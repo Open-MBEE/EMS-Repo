d31b0dbbdc17f314981afa8466aff68a1e3bc2a2
Bradley J. Clement <bclement@ljpl.nasa.gov>
2015-09-18 14:11:35 -0700

    minor cleanup

--- src/main/java/gov/nasa/jpl/view_repo/util/WorkspaceDiff.java
+++ src/main/java/gov/nasa/jpl/view_repo/util/WorkspaceDiff.java
@@ -11,7 +11,6 @@ import java.util.ArrayList;
 import java.io.Serializable;
 import java.util.Collection;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.List;
