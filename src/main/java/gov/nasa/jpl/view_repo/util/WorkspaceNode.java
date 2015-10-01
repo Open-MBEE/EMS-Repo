b434527fc06fa87291f7a98a663565c428df9165
Bradley J. Clement <bclement@ljpl.nasa.gov>
2015-09-30 18:00:51 -0700

    comment for branch time

--- src/main/java/gov/nasa/jpl/view_repo/util/WorkspaceNode.java
+++ src/main/java/gov/nasa/jpl/view_repo/util/WorkspaceNode.java
@@ -411,8 +411,13 @@ public class WorkspaceNode extends EmsScriptNode {
         //String thisName = exists() ? getName() : null;
         String nodeName = node != null && node.scriptNodeExists() ? node.getName() : null;
 
-        // make sure the folder's parent is replicated
-        EmsScriptNode parent = node.getParent(null, node.getWorkspace(), false, true);
+        Pair< WorkspaceNode, Date > pr = WorkspaceDiff.getCommonBranchPoint( this, node.getWorkspace(), (Date)null, (Date)null );
+        Date commonBranchPoint = pr.second;
+
+        // Make sure the folder's parent is replicated. We get the parent at the
+        // common branch point because changes to the parent after the branch
+        // time should be ignored.
+        EmsScriptNode parent = node.getParent(commonBranchPoint, node.getWorkspace(), false, true);
 
         if ( parent == null || parent.isWorkspaceTop() ) {
             parent = this; // put in the workspace
