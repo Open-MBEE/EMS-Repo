b7881c1aef8d068670934976c742d0b64b559050
Dan Karlsson <dan.e.karlsson@jpl.nasa.gov>
2015-10-21 06:02:50 -0700

    Set up view repo to make the service for finding all version history of an element.

--- src/main/java/gov/nasa/jpl/view_repo/webscripts/HistoryGet.java
+++ src/main/java/gov/nasa/jpl/view_repo/webscripts/HistoryGet.java
@@ -3,95 +3,39 @@
  */
 package gov.nasa.jpl.view_repo.webscripts;
 
-import gov.nasa.jpl.mbee.util.ClassUtils;
-import gov.nasa.jpl.mbee.util.CompareUtils;
-import gov.nasa.jpl.mbee.util.CompareUtils.GenericComparator;
-import gov.nasa.jpl.mbee.util.Debug;
-import gov.nasa.jpl.mbee.util.MethodCall;
-import gov.nasa.jpl.mbee.util.Pair;
-import gov.nasa.jpl.mbee.util.TimeUtils;
-import gov.nasa.jpl.mbee.util.Timer;
-import gov.nasa.jpl.mbee.util.Utils;
-import gov.nasa.jpl.view_repo.actions.ActionUtil;
-import gov.nasa.jpl.view_repo.util.EmsScriptNode.EmsVersion;
-
-import java.io.ByteArrayInputStream;
-import java.io.InputStream;
-import java.io.Serializable;
-import java.lang.reflect.Method;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.LinkedHashSet;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.TreeSet;
-import java.util.UUID;
-import java.util.Vector;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import javax.transaction.UserTransaction;
-import javax.xml.bind.DatatypeConverter;
+import javax.servlet.http.HttpServletResponse;
 
-import org.alfresco.model.ContentModel;
-import org.alfresco.repo.domain.node.NodeDAO;
-import org.alfresco.repo.jscript.ScriptNode;
-import org.alfresco.repo.jscript.ScriptVersion;
 import org.alfresco.repo.model.Repository;
-import org.alfresco.repo.node.db.DbNodeServiceImpl;
-import org.alfresco.repo.security.authentication.AuthenticationUtil;
 import org.alfresco.service.ServiceRegistry;
-import org.alfresco.service.cmr.dictionary.AspectDefinition;
-import org.alfresco.service.cmr.dictionary.DictionaryService;
-import org.alfresco.service.cmr.dictionary.PropertyDefinition;
-import org.alfresco.service.cmr.repository.ContentData;
-import org.alfresco.service.cmr.repository.ContentWriter;
-import org.alfresco.service.cmr.repository.InvalidNodeRefException;
 import org.alfresco.service.cmr.repository.NodeRef;
-import org.alfresco.service.cmr.repository.NodeService;
-import org.alfresco.service.cmr.repository.StoreRef;
-import org.alfresco.service.cmr.search.LimitBy;
-import org.alfresco.service.cmr.search.ResultSet;
-import org.alfresco.service.cmr.search.ResultSetRow;
-import org.alfresco.service.cmr.search.SearchParameters;
-import org.alfresco.service.cmr.search.SearchService;
-import org.alfresco.service.cmr.security.AccessStatus;
-import org.alfresco.service.cmr.security.AuthorityService;
-import org.alfresco.service.cmr.security.AuthorityType;
-import org.alfresco.service.cmr.security.PermissionService;
-import org.alfresco.service.cmr.security.PersonService;
-import org.alfresco.service.cmr.site.SiteInfo;
-import org.alfresco.service.cmr.version.Version;
 import org.alfresco.service.cmr.version.VersionHistory;
-import org.alfresco.service.namespace.QName;
-import org.alfresco.util.ApplicationContextHelper;
-import org.apache.commons.logging.Log;
+import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.springframework.context.ApplicationContext;
 import org.springframework.extensions.webscripts.Cache;
 import org.springframework.extensions.webscripts.Status;
 import org.springframework.extensions.webscripts.WebScriptRequest;
+import gov.nasa.jpl.view_repo.webscripts.ModelGet;
+import gov.nasa.jpl.mbee.util.TimeUtils;
+import gov.nasa.jpl.view_repo.util.EmsScriptNode;
+import gov.nasa.jpl.view_repo.util.NodeUtil;
+import gov.nasa.jpl.view_repo.util.WorkspaceNode;
 
 /**
  * @author dank
  *
  */
-public class HistoryGet extends AbstractJavaWebScript{
+public class HistoryGet extends ModelGet {
 
 	static Logger logger = Logger.getLogger(HistoryGet.class);
+
 	/**
 	 * 
 	 */
@@ -99,33 +43,125 @@ public class HistoryGet extends AbstractJavaWebScript{
 		// TODO Auto-generated constructor stub
 		super();
 	}
-	
-	public HistoryGet( Repository repositoryHelper, ServiceRegistry registry){
-		super( repositoryHelper, registry);
+
+	/**
+	 * 
+	 * @param repositoryHelper
+	 * @param registry
+	 */
+	public HistoryGet(Repository repositoryHelper, ServiceRegistry registry) {
+		super(repositoryHelper, registry);
 	}
 
-	
-	public JSONObject getAllHistory(NodeRef ref){
+	/**
+	 * 
+	 * @param ref
+	 * @return
+	 */
+	public JSONObject getAllHistory(NodeRef ref) {
 		VersionHistory history = null;
 		JSONObject jsonHistory = null;
-		
 		history = getServices().getVersionService().getVersionHistory(ref);
 		jsonHistory = new JSONObject(history);
-		
 		System.out.println(jsonHistory.toString());
 		return jsonHistory;
-		
 	}
 
-	@Override
 	protected Map<String, Object> executeImplImpl(WebScriptRequest req, Status status, Cache cache) {
-		HistoryGet instance = new HistoryGet( repository, getServices());
-		return instance.executeImplImpl(req, status,cache,runWithoutTransactions);
+		Map<String, Object> history = new HashMap<String, Object>();
+		JSONObject top = NodeUtil.newJsonObject();
+		JSONArray historyJson = handleRequest(req, top);
+
+		// Execute methods in here as if it is main
+		return history;
 	}
+	// @Override
+	// protected Map<String, Object> executeImpl(WebScriptRequest req, Status
+	// status, Cache cache) {
+	// HistoryGet instance = new HistoryGet( repository, getServices());
+	//
+	// // Execute methods in here as if it is main
+	// return instance.executeImplImpl(req,
+	// status,cache,runWithoutTransactions);
+	// }
 
 	@Override
 	protected boolean validateRequest(WebScriptRequest req, Status status) {
 		// TODO Auto-generated method stub
 		return true;
+	}
+
+	/**
+	 * Wrapper for handling a request and getting the appropriate JSONArray of
+	 * elements
+	 * 
+	 * @param req
+	 * @param top
+	 * @return
+	 */
+	private JSONArray handleRequest(WebScriptRequest req, final JSONObject top) {
+		// REVIEW -- Why check for errors here if validate has already been
+		// called? Is the error checking code different? Why?
+		try {
+			String[] idKeys = { "modelid", "elementid", "elementId" };
+			String modelId = null;
+			for (String idKey : idKeys) {
+				modelId = req.getServiceMatch().getTemplateVars().get(idKey);
+				if (modelId != null) {
+					break;
+				}
+			}
+
+			if (null == modelId) {
+				log(Level.ERROR, HttpServletResponse.SC_NOT_FOUND, "Could not find element %s", modelId);
+				return new JSONArray();
+			}
+
+			// get timestamp if specified
+			String timestamp = req.getParameter("timestamp");
+			Date dateTime = TimeUtils.dateFromTimestamp(timestamp);
+			boolean connected = getBooleanArg(req, "connected", false);
+			boolean evaluate = getBooleanArg(req, "evaluate", false);
+			String relationship = req.getParameter("relationship");
+
+			WorkspaceNode workspace = getWorkspace(req);
+
+			// see if prettyPrint default is overridden and change
+			prettyPrint = getBooleanArg(req, "pretty", prettyPrint);
+
+			Long depth = 0l;
+			boolean includeQualified = getBooleanArg(req, "qualified", true);
+
+			if (logger.isDebugEnabled())
+				logger.debug("modelId = " + modelId);
+			boolean findDeleted = depth == 0 ? true : false;
+			EmsScriptNode modelRootNode = findScriptNodeById(modelId, workspace, dateTime, findDeleted);
+			if (logger.isDebugEnabled())
+				logger.debug("modelRootNode = " + modelRootNode);
+
+			if (modelRootNode == null) {
+				log(Level.ERROR, HttpServletResponse.SC_NOT_FOUND, "Element %s not found",
+						modelId + (dateTime == null ? "" : " at " + dateTime));
+				return new JSONArray();
+			} else if (modelRootNode.isDeleted()) {
+				log(Level.DEBUG, HttpServletResponse.SC_GONE, "Element exists, but is deleted.");
+				return new JSONArray();
+			}
+
+			if (isViewRequest) {
+				handleViewHierarchy(modelRootNode, workspace, dateTime, depth, new Long(0));
+			} else {
+				handleElementHierarchy(modelRootNode, workspace, dateTime, depth, new Long(0), connected, relationship,
+						new HashSet<String>());
+			}
+
+			boolean checkReadPermission = true; // TODO -- REVIEW -- Shouldn't
+												// this be false?
+			handleElements(workspace, dateTime, includeQualified, evaluate, top, checkReadPermission);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
+		return elements;
 	}
 }
