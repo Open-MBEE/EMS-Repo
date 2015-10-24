e14316d158f64b0cc5ec86c22f20fe62cd0a7b4b
Dan Karlsson <dan.e.karlsson@jpl.nasa.gov>
2015-10-23 21:32:44 -0700

    Added service for searching for version history of a particular element. It returns the time of when node freezing point.

--- src/main/java/gov/nasa/jpl/view_repo/webscripts/HistoryGet.java
+++ src/main/java/gov/nasa/jpl/view_repo/webscripts/HistoryGet.java
@@ -7,15 +7,20 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Vector;
+import java.util.Collection;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.alfresco.repo.model.Repository;
+import org.alfresco.repo.security.authentication.AuthenticationUtil;
 import org.alfresco.service.ServiceRegistry;
 import org.alfresco.service.cmr.repository.NodeRef;
+import org.alfresco.service.cmr.version.Version;
 import org.alfresco.service.cmr.version.VersionHistory;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
+//import org.hibernate.mapping.Collection;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -24,6 +29,8 @@ import org.springframework.extensions.webscripts.Status;
 import org.springframework.extensions.webscripts.WebScriptRequest;
 import gov.nasa.jpl.view_repo.webscripts.ModelGet;
 import gov.nasa.jpl.mbee.util.TimeUtils;
+import gov.nasa.jpl.mbee.util.Timer;
+import gov.nasa.jpl.mbee.util.Utils;
 import gov.nasa.jpl.view_repo.util.EmsScriptNode;
 import gov.nasa.jpl.view_repo.util.NodeUtil;
 import gov.nasa.jpl.view_repo.util.WorkspaceNode;
@@ -60,54 +67,90 @@ public class HistoryGet extends ModelGet {
 	 * @param ref
 	 * @return
 	 */
-	public JSONObject getAllHistory(NodeRef ref) {
-		System.out.println("getAllHistory...");
+	public JSONArray getAllHistory(NodeRef ref) {
+		JSONArray jsonAr = null;
 		VersionHistory history = null;
-		JSONObject jsonHistory = null;
-		history = getServices().getVersionService().getVersionHistory(ref);
-		jsonHistory = new JSONObject(history);
-		System.out.println(jsonHistory.toString());
-		return jsonHistory;
+		// Tries to to get the version history based on the NodeRef that is
+		// passed into the
+		// getVersionHistory method.
+		try {
+			history = getServices().getVersionService().getVersionHistory(ref);
+		} catch (Exception error) {
+			System.out.println(error.getMessage());
+		}
+		Collection<Version> versions = history.getAllVersions();
+
+		if (versions != null) {
+			jsonAr = new JSONArray(versions);
+		}
+
+		return jsonAr;
 	}
-    protected Map< String, Object > executeImpl( WebScriptRequest req,Status status, Cache cache ) {
-    		HistoryGet instance = new HistoryGet( repository, getServices() );
-    		return instance.executeImplImpl( req, status, cache,runWithoutTransactions );
-}
+
+	protected Map<String, Object> executeImpl(WebScriptRequest req, Status status, Cache cache) {
+		HistoryGet instance = new HistoryGet(repository, getServices());
+		return instance.executeImplImpl(req, status, cache, runWithoutTransactions);
+	}
+
 	
 	protected Map<String, Object> executeImplImpl(WebScriptRequest req, Status status, Cache cache) {
-		Map<String, Object> history = new HashMap<String, Object>();
-//		JSONObject top = NodeUtil.newJsonObject();
-//		JSONArray historyJson = handleRequest(req, top);
-		System.out.println("executeImplImpl...");
-		
-//		if(historyJson != null)
-//		{
-//			log(Level.ERROR, HttpServletResponse.SC_BAD_REQUEST,historyJson.toString());
-//			System.out.println(historyJson.toString());
-//		}
-		EmsScriptNode nodeRef;
-		
-		nodeRef = getSiteNodeFromRequest(req, true);	
-		if(nodeRef == null)
-		{
-			System.out.println("NodeRef Null...");
-			return null;
+
+		int index;
+		Map<String, Object> model = new HashMap<String, Object>();
+
+		if (logger.isDebugEnabled()) {
+			String user = AuthenticationUtil.getFullyAuthenticatedUser();
+			logger.debug(user + " " + req.getURL());
 		}
-		
-//		this.getAllHistory(nodeRef.getNodeRef());
-		
-		// Execute methods in here as if it is main
-		return history;
+		Timer timer = new Timer();
+
+		// make sure to pass down view request flag to instance
+		setIsViewRequest(isViewRequest);
+
+		// Creates a JSON object of NodeUtil which is passed into handleRquest along with the request - 'req'. 
+		// 	HandleRequest will find all NodeRefs that belong with the request then search for all version histories of that
+		//	element. It will returns a JSONArray to be parsed for the keys versionLabel and frozenCreated. The values
+		//	associated with these keys will be put to a json object that is a appended to an array. Once all values have
+		//	been extracted, it maps the array and returns the Map.
+		JSONObject top = NodeUtil.newJsonObject();
+		JSONArray elementsJson = handleRequest(req, top);
+
+		try {
+			JSONObject obj = new JSONObject();
+			top = new JSONObject();
+			
+			// Searches for each key and value to be appended to the json array.
+			for (index = 0; index < elementsJson.length(); index++) {
+				obj.put("label",
+						elementsJson.getJSONObject(index).getJSONObject("versionProperties").get("versionLabel"));
+				obj.put("timestamp",
+						elementsJson.getJSONObject(index).getJSONObject("versionProperties").get("frozenCreated"));
+				top.append("versions", obj);
+				obj = new JSONObject();
+			}
+
+			if (!Utils.isNullOrEmpty(response.toString()))
+				top.put("message", response.toString());
+			if (prettyPrint) {
+				model.put("res", NodeUtil.jsonToString(top, 4));
+			} else {
+				model.put("res", NodeUtil.jsonToString(top));
+			}
+		} catch (JSONException e) {
+			log(Level.ERROR, HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Could not create JSONObject");
+			model.put("res", createResponseJson());
+			e.printStackTrace();
+		}
+
+		status.setCode(responseStatus.getCode());
+		printFooter();
+
+		if (logger.isInfoEnabled()) {
+			log(Level.INFO, "ModelGet: %s", timer);
+		}
+
+		return model;
 	}
-	// @Override
-	// protected Map<String, Object> executeImpl(WebScriptRequest req, Status
-	// status, Cache cache) {
-	// HistoryGet instance = new HistoryGet( repository, getServices());
-	//
-	// // Execute methods in here as if it is main
-	// 	return instance.executeImplImpl(req,
-	// status,cache,runWithoutTransactions);
-	// }
 
 	@Override
 	protected boolean validateRequest(WebScriptRequest req, Status status) {
@@ -123,71 +166,62 @@ public class HistoryGet extends ModelGet {
 	 * @param top
 	 * @return
 	 */
-//	private JSONArray handleRequest(WebScriptRequest req, final JSONObject top) {
-//		// REVIEW -- Why check for errors here if validate has already been
-//		// called? Is the error checking code different? Why?
-//		System.out.println("Handle Request...");
-//		try {
-//			String[] idKeys = { "modelid", "elementid", "elementId" };
-//			String modelId = null;
-//			for (String idKey : idKeys) {
-//				modelId = req.getServiceMatch().getTemplateVars().get(idKey);
-//				if (modelId != null) {
-//					break;
-//				}
-//			}
-//
-//			if (null == modelId) {
-//				System.out.println("Model ID Null...");
-//				log(Level.ERROR, HttpServletResponse.SC_NOT_FOUND, "Could not find element %s", modelId);
-//				return new JSONArray();
-//			}
-//
-//			// get timestamp if specified
-//			String timestamp = req.getParameter("timestamp");
-//			Date dateTime = TimeUtils.dateFromTimestamp(timestamp);
-//			boolean connected = getBooleanArg(req, "connected", false);
-//			boolean evaluate = getBooleanArg(req, "evaluate", false);
-//			String relationship = req.getParameter("relationship");
-//
-//			WorkspaceNode workspace = getWorkspace(req);
-//
-//			// see if prettyPrint default is overridden and change
-//			prettyPrint = getBooleanArg(req, "pretty", prettyPrint);
-//
-//			Long depth = 0l;
-//			boolean includeQualified = getBooleanArg(req, "qualified", true);
-//
-//			if (logger.isDebugEnabled())
-//				logger.debug("modelId = " + modelId);
-//			boolean findDeleted = depth == 0 ? true : false;
-//			EmsScriptNode modelRootNode = findScriptNodeById(modelId, workspace, dateTime, findDeleted);
-//			if (logger.isDebugEnabled())
-//				logger.debug("modelRootNode = " + modelRootNode);
-//
-//			if (modelRootNode == null) {
-//				log(Level.ERROR, HttpServletResponse.SC_NOT_FOUND, "Element %s not found",
-//						modelId + (dateTime == null ? "" : " at " + dateTime));
-//				return new JSONArray();
-//			} else if (modelRootNode.isDeleted()) {
-//				log(Level.DEBUG, HttpServletResponse.SC_GONE, "Element exists, but is deleted.");
-//				return new JSONArray();
-//			}
-//
-//			if (isViewRequest) {
-//				handleViewHierarchy(modelRootNode, workspace, dateTime, depth, new Long(0));
-//			} else {
-//				handleElementHierarchy(modelRootNode, workspace, dateTime, depth, new Long(0), connected, relationship,
-//						new HashSet<String>());
-//			}
-//
-//			boolean checkReadPermission = true; // TODO -- REVIEW -- Shouldn't
-//												// this be false?
-//			handleElements(workspace, dateTime, includeQualified, evaluate, top, checkReadPermission);
-//		} catch (JSONException e) {
-//			e.printStackTrace();
-//		}
-//
-//		return elements;
-//	}
+	private JSONArray handleRequest(WebScriptRequest req, final JSONObject top) {
+		// REVIEW -- Why check for errors here if validate has already been
+		// called? Is the error checking code different? Why?
+
+		// Creates an empty JSONArray
+		JSONArray jsonHist = null;
+		try {
+			String[] idKeys = { "modelid", "elementid", "elementId" };
+			String modelId = null;
+			for (String idKey : idKeys) {
+				modelId = req.getServiceMatch().getTemplateVars().get(idKey);
+				if (modelId != null) {
+					break;
+				}
+			}
+
+			if (null == modelId) {
+				System.out.println("Model ID Null...");
+				log(Level.ERROR, HttpServletResponse.SC_NOT_FOUND, "Could not find element %s", modelId);
+				return new JSONArray();
+			}
+
+			// get timestamp if specified
+			String timestamp = req.getParameter("timestamp");
+			Date dateTime = TimeUtils.dateFromTimestamp(timestamp);
+
+			WorkspaceNode workspace = getWorkspace(req);
+
+			// see if prettyPrint default is overridden and change
+			prettyPrint = getBooleanArg(req, "pretty", prettyPrint);
+
+			Long depth = 0l;
+
+			if (logger.isDebugEnabled())
+				logger.debug("modelId = " + modelId);
+			boolean findDeleted = depth == 0 ? true : false;
+			EmsScriptNode modelRootNode = findScriptNodeById(modelId, workspace, dateTime, findDeleted);
+			if (logger.isDebugEnabled())
+				logger.debug("modelRootNode = " + modelRootNode);
+
+			if (modelRootNode == null) {
+				log(Level.ERROR, HttpServletResponse.SC_NOT_FOUND, "Element %s notfound",
+						modelId + (dateTime == null ? "" : " at " + dateTime));
+				return new JSONArray();
+			} else if (modelRootNode.isDeleted()) {
+				log(Level.DEBUG, HttpServletResponse.SC_GONE, "Element exists, but is deleted.");
+				return new JSONArray();
+			}
+
+			// returns a JSONArray of versionedNodeRef JSONObjects 
+			jsonHist = getAllHistory(modelRootNode.getNodeRef());
+
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+
+		return jsonHist;
+	}
 }
