5960019fd3a7a5cc2146df76f6960f760295a464
Jason Han <jason.han@jpl.nasa.gov>
2016-01-11 17:04:27 -0800

    Clean cpr.sh

--- cpr.sh
+++ cpr.sh
@@ -5,6 +5,4 @@ echo psql -U $USER -f ./src/main/java/gov/nasa/jpl/view_repo/db/mms.sql
 psql -U mmsuser -f ./src/main/java/gov/nasa/jpl/view_repo/db/mms.sql
 #Configurations that need a suffix with a database name to run psql command
 psql -U mmsuser -f ./src/main/java/gov/nasa/jpl/view_repo/db/mms.sql mydb
-psql -U mmsuser mms -f ./src/main/java/gov/nasa/jpl/view_repo/db/mms.sql
 mvn jrebel:generate
-./runserver.sh 
