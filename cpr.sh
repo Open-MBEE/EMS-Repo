7d9644ea0e2b5c1f970f486d9c0e825679eb1c88
Jason Han <jason.han@jpl.nasa.gov>
2016-01-11 17:06:40 -0800

    Revert "Clean cpr.sh"
    This reverts commit 2182c679563b509fd00e3d53d12a43380ca279e8.

--- cpr.sh
+++ cpr.sh
@@ -8,3 +8,4 @@ psql -U mmsuser -f ./src/main/java/gov/nasa/jpl/view_repo/db/mms.sql
 #Configurations that need a suffix with a database name to run psql command
 psql -U mmsuser -f ./src/main/java/gov/nasa/jpl/view_repo/db/mms.sql mydb
 mvn jrebel:generate
+./runserver.sh 
