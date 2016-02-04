fb19d9bf3ff511686effa49e997d25b3826ad24d
cinyoung <cinyoung@jpl.nasa.gov>
2016-02-03 22:24:59 -0800

    Handle null histories in HistoryGet.

--- src/main/java/gov/nasa/jpl/view_repo/webscripts/HistoryGet.java
+++ src/main/java/gov/nasa/jpl/view_repo/webscripts/HistoryGet.java
@@ -75,6 +75,11 @@ public class HistoryGet extends ModelGet {
         } catch (Exception error) {
             logger.error(error.getMessage());
         }
+        if (history == null) {
+            logger.warn("couldn't get history for: " + ref);
+            return jsonAr;
+        }
+        
         Collection<Version> versions = history.getAllVersions();
 
         if (versions != null) {
