8a44f67504b78452f446524c15d1c4ae108fca59
Jason Han <jason.han@jpl.nasa.gov>
2016-02-04 09:06:53 -0800

    Revert cpr.sh

--- cpr.sh
+++ cpr.sh
@@ -2,6 +2,7 @@
 
 mvn clean -P purge
 echo psql -U $USER -f ./src/main/java/gov/nasa/jpl/view_repo/db/mms.sql
+psql -U mmsuser -f ./src/main/java/gov/nasa/jpl/view_repo/db/mms.sql
 #Configurations that need a suffix with a database name to run psql command
 psql -U mmsuser mms -f ./src/main/java/gov/nasa/jpl/view_repo/db/mms.sql
 psql -U mmsuser mms -f ./src/main/java/gov/nasa/jpl/view_repo/db/doors-mms.sql
