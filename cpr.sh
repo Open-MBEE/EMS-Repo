6883a8851e8d437840dc1ad7ce9b7601a7553691
Bradley J. Clement <bclement@ljpl.nasa.gov>
2016-02-07 00:38:10 -0800

    fix compile error; update baseline for SolveConstraint

--- cpr.sh
+++ cpr.sh
@@ -2,6 +2,8 @@
 
 mvn clean -P purge
 echo psql -U $USER -f ./src/main/java/gov/nasa/jpl/view_repo/db/mms.sql
+psql -U $USER -f ./src/main/java/gov/nasa/jpl/view_repo/db/mms.sql mydb
+psql -U $USER -f ./src/main/java/gov/nasa/jpl/view_repo/db/mms.sql mydb
 psql -U mmsuser -f ./src/main/java/gov/nasa/jpl/view_repo/db/mms.sql
 #Configurations that need a suffix with a database name to run psql command
 psql -U mmsuser -f ./src/main/java/gov/nasa/jpl/view_repo/db/mms.sql mydb
