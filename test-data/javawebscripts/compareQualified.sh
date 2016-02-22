18e861e1d2ee3cb1229f7f630ed8e6e6b9138006
cinyoung <cinyoung@jpl.nasa.gov>
2016-02-22 08:50:15 -0800

    CAEDVO-2987: Utility scripts for diffing between Baseline and Result.

--- test-data/javawebscripts/compareQualified.sh
+++ test-data/javawebscripts/compareQualified.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+# Utility for checking the sysmlids between Baseline and Result
 
-grep qualifiedId developBaselineDir/$1 | sort > t1.json
-grep qualifiedId developResultDir/$1 | sort > t2.json
+grep sysmlid developBaselineDir/$1.json | sort > t1.json
+grep sysmlid developResultDir/$1.json | sort > t2.json
 
 tkdiff t1.json t2.json
