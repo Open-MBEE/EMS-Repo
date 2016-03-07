459396434d5cdfe81b0a7f759775328fd53c182f
cinyoung <cinyoung@jpl.nasa.gov>
2016-03-07 11:25:09 -0800

    updates to work on cae-leng

--- cpr.sh
+++ cpr.sh
@@ -2,9 +2,9 @@
 
 mvn clean -P purge
 echo psql -U $USER -f ./src/main/java/gov/nasa/jpl/view_repo/db/mms.sql
-psql -U mmsuser -f ./src/main/java/gov/nasa/jpl/view_repo/db/mms.sql
+psql -U postgres -f ./src/main/java/gov/nasa/jpl/view_repo/db/mms.sql
 #Configurations that need a suffix with a database name to run psql command
-psql -U mmsuser -f ./src/main/java/gov/nasa/jpl/view_repo/db/mms.sql mms
-psql -U mmsuser -f ./src/main/java/gov/nasa/jpl/view_repo/db/doors-mms.sql mms
+psql -U postgres -f ./src/main/java/gov/nasa/jpl/view_repo/db/mms.sql mydb
+psql -U postgres -f ./src/main/java/gov/nasa/jpl/view_repo/db/doors-mms.sql mydb
 mvn jrebel:generate
 ./runserver.sh 
