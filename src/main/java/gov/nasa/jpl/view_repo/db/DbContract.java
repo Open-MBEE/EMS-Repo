9d34210ec3a37345ff32dc564246e392d1aac60c
reichwei <axel.reichwein@jpl.nasa.gov>
2016-03-08 15:38:56 -0800

    - updated name of postgres user

--- src/main/java/gov/nasa/jpl/view_repo/db/DbContract.java
+++ src/main/java/gov/nasa/jpl/view_repo/db/DbContract.java
@@ -4,6 +4,6 @@ public interface DbContract {
     // localhost sometimes is mapped to, so the SQL driver needs 127.0.0.1 in HOST
 	public static final String HOST = "jdbc:postgresql://127.0.0.1/";
 	public static final String DB_NAME = "mms";
-	public static final String USERNAME = "mmsuser";
+	public static final String USERNAME = "postgres";
 	public static final String PASSWORD = "test123";
 }
