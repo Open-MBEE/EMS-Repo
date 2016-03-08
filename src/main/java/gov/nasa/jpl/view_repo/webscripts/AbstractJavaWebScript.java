79bfffa6c810ab5e67206634e9145889f4f3852e
Patrick Perey <patrick.r.perey@jpl.nasa.gov>
2016-03-08 13:37:05 -0800

    pushing local to remote

--- src/main/java/gov/nasa/jpl/view_repo/webscripts/AbstractJavaWebScript.java
+++ src/main/java/gov/nasa/jpl/view_repo/webscripts/AbstractJavaWebScript.java
@@ -2979,15 +2979,6 @@ public abstract class AbstractJavaWebScript extends DeclarativeJavaWebScript {
         return null;
     }
     
-    protected JSONObject jobOrEle( EmsScriptNode element, WorkspaceNode ws,
-                                   Date dateTime, String id,
-                                   boolean includeQualified,
-                                   boolean isIncludeDocument ) {
-        return element.toJSONObject( ws, dateTime, includeQualified,
-                                     isIncludeDocument,
-                                     elementProperties.get( id ) );
-    }
-    
     public void postProcessJson( JSONObject top ) {
         // redefine this if you want to add jobs or other things; see
         // JobGet.postProcessJson()
