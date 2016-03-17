296b3109e58bcfbf6f4a8448215a21cf0ecbe52e
Bradley J. Clement <bclement@ljpl.nasa.gov>
2016-03-16 20:09:08 -0700

    fixed includeQualified argument to toJSONObject()

--- src/main/java/gov/nasa/jpl/view_repo/webscripts/ModelPost.java
+++ src/main/java/gov/nasa/jpl/view_repo/webscripts/ModelPost.java
@@ -2898,7 +2898,7 @@ public class ModelPost extends AbstractJavaWebScript {
                                                               0, false );
                             }
                             if ( json == null ) {
-                                boolean isQualified = NodeUtil.doPostProcessQualified;
+                                boolean isQualified = !NodeUtil.doPostProcessQualified;
                                 json = getJsonForElementAndJob( element, null, false, workspace, 
                                                           null, element.getSysmlId(), 
                                                           isQualified, false );
