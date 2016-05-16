2e21276502383fa3b8bfa1f96854ca7aa4ce8395
BMJ <services@indefinite-domain.com>
2016-05-16 04:05:28 -0400

    DbContract no longer needed

--- src/main/java/gov/nasa/jpl/view_repo/db/DbContract.java
+++
@@ -1,9 +0,0 @@
-package gov.nasa.jpl.view_repo.db;
-
-public interface DbContract {
-    // localhost sometimes is mapped to, so the SQL driver needs 127.0.0.1 in HOST
-	public static final String HOST = "jdbc:postgresql://127.0.0.1/";
-	public static final String DB_NAME = "mms";
-	public static final String USERNAME = "postgres";
-	public static final String PASSWORD = "test123";
-}
